0c29b01755485c6ceadc557b26c90b9c7c9abb7f
isratmity01 <mity.crystal@gmail.com>
2016-01-31 15:33:43 +0600

    fixed crash issue for older device;feedbackactivity on processing

---
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/activity/FeedbackActivity.java
@@ -0,0 +1,35 @@
+package demo.kolorob.kolorobdemoversion.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import demo.kolorob.kolorobdemoversion.R;
+
+public class FeedbackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_feedback);
+        ArrayList<String> years = new ArrayList<String>();
+        int thisYear = Calendar.getInstance().get(Calendar.YEAR);
+        for (int i = 1900; i <= thisYear; i++) {
+            years.add(Integer.toString(i));
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, years);
+
+        Spinner spinYear = (Spinner)findViewById(R.id.spinner);
+        spinYear.setAdapter(adapter);
+
+    }
+
+}
