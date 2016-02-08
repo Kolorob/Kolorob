eaefb04d154fcc3e577a9e7356c993bfce0cb95f
isratmity01 <mity.crystal@gmail.com>
2016-02-08 16:01:46 +0600

    Model and database done for Education

--- app/src/main/java/demo/kolorob/kolorobdemoversion/database/Education/EducationCourseInfoTable.java
+++
@@ -1,7 +0,0 @@
-package demo.kolorob.kolorobdemoversion.database.Education;
-
-/**
- * Created by Yeakub Hassan Rafi on 30-Dec-15.
- */
-public class EducationCourseInfoTable {
-}
---
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/database/Education/EducationCourseTable.java
@@ -0,0 +1,7 @@
+package demo.kolorob.kolorobdemoversion.database.Education;
+
+/**
+ * Created by israt.jahan on 2/8/2016.
+ */
+public class EducationCourseTable {
+}
