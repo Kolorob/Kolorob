87a4aab2f9d49e5625454cca0e407622ec915b16
isratmity01 <mity.crystal@gmail.com>
2016-02-09 11:45:25 +0600

    Completed models for entertainment

--- app/src/main/java/demo/kolorob/kolorobdemoversion/model/Entertainment/EntertainmentFitnessItem.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/model/Entertainment/EntertainmentFitnessItem.java
@@ -1,7 +1,129 @@
 package demo.kolorob.kolorobdemoversion.model.Entertainment;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by israt.jahan on 2/8/2016.
  */
 public class EntertainmentFitnessItem {
+
+    private String nodeId;
+    private int entSubCategoryId;
+    private int yearofestablishment;
+    private int workers;
+    private String offers;
+    private String offerdetails;
+    private String servicetype;
+    private String type;
+    private String servicedetails;
+
+    public EntertainmentFitnessItem(String nodeId, int entSubCategoryId, int yearofestablishment, int workers, String offers, String offerdetails, String servicetype, String type, String servicedetails) {
+        this.nodeId = nodeId;
+        this.entSubCategoryId = entSubCategoryId;
+        this.yearofestablishment = yearofestablishment;
+        this.workers = workers;
+        this.offers = offers;
+        this.offerdetails = offerdetails;
+        this.servicetype = servicetype;
+        this.type = type;
+        this.servicedetails = servicedetails;
+    }
+
+    public String getNodeId() {
+        return nodeId;
+    }
+
+    public void setNodeId(String nodeId) {
+        this.nodeId = nodeId;
+    }
+
+    public int getEntSubCategoryId() {
+        return entSubCategoryId;
+    }
+
+    public void setEntSubCategoryId(int entSubCategoryId) {
+        this.entSubCategoryId = entSubCategoryId;
+    }
+
+    public int getYearofestablishment() {
+        return yearofestablishment;
+    }
+
+    public void setYearofestablishment(int yearofestablishment) {
+        this.yearofestablishment = yearofestablishment;
+    }
+
+    public int getWorkers() {
+        return workers;
+    }
+
+    public void setWorkers(int workers) {
+        this.workers = workers;
+    }
+
+    public String getOffers() {
+        return offers;
+    }
+
+    public void setOffers(String offers) {
+        this.offers = offers;
+    }
+
+    public String getOfferdetails() {
+        return offerdetails;
+    }
+
+    public void setOfferdetails(String offerdetails) {
+        this.offerdetails = offerdetails;
+    }
+
+    public String getServicetype() {
+        return servicetype;
+    }
+
+    public void setServicetype(String servicetype) {
+        this.servicetype = servicetype;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getServicedetails() {
+        return servicedetails;
+    }
+
+    public void setServicedetails(String servicedetails) {
+        this.servicedetails = servicedetails;
+    }
+
+    public static EntertainmentFitnessItem parseEntertainmentFitnessItem(JSONObject jo) throws JSONException {
+        String _nodeId = jo.getString("node_id");
+        int _entSubCategoryId = jo.getInt("ent_sub_category_id");
+        int _yearofestablishment = jo.getInt("year_of_establishment");
+        int _workers = jo.getInt("num_workers");
+        String _offers = jo.getString("offers");
+        String _offerdetails = jo.getString("offer_details");
+        String _servicetype = jo.getString("service_type");
+        String _type = jo.getString("type");
+        String _servicedetails = jo.getString("service_details");
+
+        return new EntertainmentFitnessItem(
+                _nodeId,
+                _entSubCategoryId,
+                _yearofestablishment,
+                _workers,
+                _offers,
+                _offerdetails,
+                _servicetype,
+                _type,
+                _servicedetails
+
+        );
+    }
 }
--- app/src/main/java/demo/kolorob/kolorobdemoversion/model/Entertainment/EntertainmentTheatreItem.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/model/Entertainment/EntertainmentTheatreItem.java
@@ -1,7 +1,126 @@
 package demo.kolorob.kolorobdemoversion.model.Entertainment;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by israt.jahan on 2/8/2016.
  */
 public class EntertainmentTheatreItem {
+    private String nodeId;
+    private int entSubCategoryId;
+    private int theatreId;
+    private String eventtype;
+    private String eventfee;
+    private String eventdate;
+    private String remarks;
+    private String eventfeeffp;
+    private String eventfeefoc;
+
+    public EntertainmentTheatreItem(String nodeId, int entSubCategoryId, int theatreId, String eventtype, String eventfee, String eventdate, String remarks, String eventfeeffp, String eventfeefoc) {
+        this.nodeId = nodeId;
+        this.entSubCategoryId = entSubCategoryId;
+        this.theatreId = theatreId;
+        this.eventtype = eventtype;
+        this.eventfee = eventfee;
+        this.eventdate = eventdate;
+        this.remarks = remarks;
+        this.eventfeeffp = eventfeeffp;
+        this.eventfeefoc = eventfeefoc;
+    }
+
+    public String getNodeId() {
+        return nodeId;
+    }
+
+    public void setNodeId(String nodeId) {
+        this.nodeId = nodeId;
+    }
+
+    public int getEntSubCategoryId() {
+        return entSubCategoryId;
+    }
+
+    public void setEntSubCategoryId(int entSubCategoryId) {
+        this.entSubCategoryId = entSubCategoryId;
+    }
+
+    public int getTheatreId() {
+        return theatreId;
+    }
+
+    public void setTheatreId(int theatreId) {
+        this.theatreId = theatreId;
+    }
+
+    public String getEventtype() {
+        return eventtype;
+    }
+
+    public void setEventtype(String eventtype) {
+        this.eventtype = eventtype;
+    }
+
+    public String getEventfee() {
+        return eventfee;
+    }
+
+    public void setEventfee(String eventfee) {
+        this.eventfee = eventfee;
+    }
+
+    public String getEventdate() {
+        return eventdate;
+    }
+
+    public void setEventdate(String eventdate) {
+        this.eventdate = eventdate;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public String getEventfeeffp() {
+        return eventfeeffp;
+    }
+
+    public void setEventfeeffp(String eventfeeffp) {
+        this.eventfeeffp = eventfeeffp;
+    }
+
+    public String getEventfeefoc() {
+        return eventfeefoc;
+    }
+
+    public void setEventfeefoc(String eventfeefoc) {
+        this.eventfeefoc = eventfeefoc;
+    }
+    public static EntertainmentTheatreItem parseEntertainmentTheatreItem(JSONObject jo) throws JSONException {
+        String _nodeId = jo.getString("node_id");
+        int _entSubCategoryId = jo.getInt("ent_sub_category_id");
+        int _theatreId = jo.getInt("theatre_id");
+        String _eventtype = jo.getString("event_type");
+        String _eventfee = jo.getString("event_fee");
+        String _eventdate = jo.getString("event_date");
+        String _remarks = jo.getString("remarks");
+        String _eventfeeffp = jo.getString("event_fee_ffp");
+        String _eventfeefoc = jo.getString("event_fee_foc");
+        return new EntertainmentTheatreItem(
+                _nodeId,
+                _entSubCategoryId,
+                _theatreId,
+                _eventtype,
+                _eventfee,
+                _eventdate,
+                _remarks,
+                _eventfeeffp,
+                _eventfeefoc
+
+        );
+    }
 }
