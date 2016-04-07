cccbfd558e575bacabab6a07eaad6ee9e5c2f57d
isratmity01 <mity.crystal@gmail.com>
2016-04-07 17:40:13 +0600

    a

--- app/src/main/java/demo/kolorob/kolorobdemoversion/activity/PlaceDetailsActivity.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/activity/PlaceDetailsActivity.java
@@ -205,7 +205,7 @@ private ListView itemList;
         texthead = (TextView) findViewById(R.id.headtext);
 
 
-        lan.setText("Bangla");
+        lan.setText("English");
 
 
 
@@ -298,7 +298,7 @@ search.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
                 if (lan.isChecked()) {
                     //Button is ON
-                    lan.setText("English");
+                    lan.setText("Bangla");
                     //Button is OFF
                     status = 1;
                     texthead.setText(R.string.Head);
@@ -308,7 +308,7 @@ search.setOnClickListener(new View.OnClickListener() {
                     SearchResult(status, currentCategoryID);
                 } else {
                     status = 0;
-                    lan.setText("Bangla");
+                    lan.setText("English");
                     texthead.setText(R.string.Headen);
                     // srad.setText("Sure Cash");
                     // brad.setText("BKash");
