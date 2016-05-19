a304eadfb4caefbed4d031814e334a8ddb3e945c
smasish <asish_cse@yahoo.com>
2016-05-19 17:03:26 +0600

    testing newdesign

--- app/src/main/java/demo/kolorob/kolorobdemoversion/activity/EmergencyAdapter.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/activity/EmergencyAdapter.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -13,8 +15,65 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import demo.kolorob.kolorobdemoversion.R;
+import demo.kolorob.kolorobdemoversion.utils.AppUtils;
 
 public class EmergencyAdapter extends ArrayAdapter<Emergency> {
+
+
+
+	String[] values = new String[] { "Bangladesh Fire Service",
+			"Police Control Room",
+			"Pllabi Police Station",
+			"RAB – 4",
+			"DESCO - Electricity",
+			"Ministry of Disaster Management","Titas Gas","Dhaka WASA","Blood Bank","Dhaka North City Corporation","Violence against Women & Children",""
+	};
+
+	String[] address = new String[] { "Mirpur 10, Dhaka",
+			"",
+			"পল্লবী থানা",
+			"",
+			"DESCO, Pallabi, Dhaka",
+			"দুর্যোগ ব্যবস্থাপনা ও ত্রাণ মন্ত্রণালয","Pllabi, Dhaka","","Red Crescent","","","",
+	};
+	String[] name_bangla = new String[] { "029555555",
+			"পুলিশ কন্ট্রোল রুম",
+			"পল্লবী থানা",
+			"র্যাব - ৪",
+			"ডেসকো – ইলেক্ট্রিসিটি ",
+			"দুর্যোগ ব্যবস্থাপনা ও ত্রাণ মন্ত্রণালয়","তিতাস গ্যাস ","ঢাকা ওয়াসা","ব্লাড ব্যাংক ","ঢাকা উত্তর সিটি কর্পোরেশন","নারী ও শিশু নির্যাতন প্রতিরোধ",""
+	};
+
+
+	String[] phone_no = new String[] { "বাংলাদেশ ফায়ার সার্ভিস অ্যান্ড সিভিল ডিফেন্স ",
+			"029555555",
+			"027124000",
+			"02-9015922",
+			"029015922",
+			"01777910499","02-9014291","16162","029139940","16364","10921",""
+	};
+
+
+	String[] address_bangla = new String[] { "",
+			"মিরপুর ১০, ঢাকা",
+			"Not Found",
+			"",
+			"পল্লবী, ঢাকা",
+			"পল্লবী, ঢাকা","","","","","","",
+	};
+
+
+	String[] phone_no_bangla = new String[] { "০২৯৫৫৫৫৫৫",
+			"০২৭১২৪০০০",
+			"০২-৯০১৫৯২২",
+			"০১৭৭৭৯১০৪৯৯",
+			"০২৯০০১০৫১",
+			"১০৯৪১","০২৯০১৪২৯১","১৬১৬২","০২৯১৩৯৯৪০","১৬৩৬৪","১০৯২১","১০৯৪১"
+	};
+
 
 	protected static final String LOG_TAG = EmergencyAdapter.class.getSimpleName();
 	
@@ -62,14 +121,19 @@ Log.d("========", "-----------------"+i);
 		//setNameTextChangeListener(holder);
 		holder.value.setOnClickListener(new View.OnClickListener() {
              public void onClick(View v) {
-                 // Perform action on click   
 
-               //  Intent activityChangeIntent = new Intent(PresentActivity.this, NextActivity.class);
+				Log.d("========", "-----------------"+i);
 
-                 // currentContext.startActivity(activityChangeIntent);
-Log.d("========", "-----------------"+i);
+				 Intent callIntent1 = new Intent(Intent.ACTION_CALL);
+				 callIntent1.setData(Uri.parse("tel:" + phone_no[i]));
+				 if(AppUtils.checkPermission(context))
+					 context.startActivity(callIntent1);
+				 else{
+					 Toast.makeText(context,
+							 "Sorry, Phone call is not possible now. ", Toast.LENGTH_LONG)
+							 .show();
+				 }
 
-               //  PresentActivity.this.startActivity(activityChangeIntent);
              }
          });
 		
