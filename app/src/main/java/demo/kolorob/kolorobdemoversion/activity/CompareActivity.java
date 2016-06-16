ed30b89fb289908afb5f5e87f60aebfa026674cf
arafat <mazharul0904055@yahoo.com>
2016-06-16 15:01:49 +0600

    Compare Tool: Bengali Text added

--- app/src/main/java/demo/kolorob/kolorobdemoversion/activity/CompareActivity.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/activity/CompareActivity.java
@@ -1,11 +1,24 @@
 package demo.kolorob.kolorobdemoversion.activity;
 
+import android.support.v4.app.SharedElementCallback;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 import demo.kolorob.kolorobdemoversion.R;
+import demo.kolorob.kolorobdemoversion.database.Education.EducationServiceProviderTable;
+import demo.kolorob.kolorobdemoversion.model.Education.EducationServiceProviderItem;
+import demo.kolorob.kolorobdemoversion.utils.SharedPreferencesHelper;
 
 public class CompareActivity extends AppCompatActivity {
+
+    private String comapreData;
+    String firstData,SecondData;
+    int checker=0;
+    EducationServiceProviderTable educationServiceProviderTable;
+    ArrayList<EducationServiceProviderItem> firstDataSet;
+    ArrayList<EducationServiceProviderItem> secondDataSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +26,54 @@ public class CompareActivity extends AppCompatActivity {
         setContentView(R.layout.activity_compare);
 
 
+        comapreData = SharedPreferencesHelper.getComapreData(CompareActivity.this);
+        int size=comapreData.length();
+        for(int i=0;i<size;i++)
+        {
+
+               if(checker==1)
+               {
+                   SecondData=SecondData+comapreData.charAt(i);
+               }
+
+               else  if(comapreData.charAt(i)==' ')
+                {
+
+                    checker=1;
+                }
+               else
+                firstData=firstData+comapreData.charAt(i);
+        }
+
+
+        educationServiceProviderTable=new EducationServiceProviderTable(CompareActivity.this);
+        firstDataSet=educationServiceProviderTable.getEducationData(Integer.parseInt(firstData));
+        secondDataSet=educationServiceProviderTable.getEducationData(Integer.parseInt(SecondData));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
 }
