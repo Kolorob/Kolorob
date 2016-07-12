d88afb76ddd81cd706ef09c6df909dff28176ad6
isratmity01 <mity.crystal@gmail.com>
2016-07-12 13:26:01 +0600

    resultmodel declared

---
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/model/Education/EducationResultItemNew.java
@@ -0,0 +1,91 @@
+package demo.kolorob.kolorobdemoversion.model.Education;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+
+/**
+ * Created by israt.jahan on 7/12/2016.
+ */
+
+
+public class EducationResultItemNew implements Serializable {
+    private int eduId;
+
+    private String examname;
+    private String studentno;
+    private String passed;
+    private String goldena;
+    private String aplus;
+
+    public EducationResultItemNew(int eduId, String examname, String studentno, String passed, String goldena, String aplus) {
+        this.eduId = eduId;
+        this.examname = examname;
+        this.studentno = studentno;
+        this.passed = passed;
+        this.goldena = goldena;
+        this.aplus = aplus;
+    }
+
+    public int getEduId() {
+        return eduId;
+    }
+
+    public void setEduId(int eduId) {
+        this.eduId = eduId;
+    }
+
+    public String getExamname() {
+        return examname;
+    }
+
+    public void setExamname(String examname) {
+        this.examname = examname;
+    }
+
+    public String getStudentno() {
+        return studentno;
+    }
+
+    public void setStudentno(String studentno) {
+        this.studentno = studentno;
+    }
+
+    public String getPassed() {
+        return passed;
+    }
+
+    public void setPassed(String passed) {
+        this.passed = passed;
+    }
+
+    public String getGoldena() {
+        return goldena;
+    }
+
+    public void setGoldena(String goldena) {
+        this.goldena = goldena;
+    }
+
+    public String getAplus() {
+        return aplus;
+    }
+
+    public void setAplus(String aplus) {
+        this.aplus = aplus;
+    }
+
+    public static EducationResultItemNew parseEducationResultItemNew(JSONObject jo) throws JSONException {
+        int _eduId = jo.getInt("id");
+
+        String _examname = jo.getString("exam_names");
+        String _studentno = jo.getString("student_no");
+        String _passed = jo.getString("passed");
+        String _goldena = jo.getString("golden_a");
+        String _aplus = jo.getString("a_plus");
+
+        return new EducationResultItemNew(_eduId,
+                _examname,_studentno,_passed,_goldena,_aplus);
+    }
+}
