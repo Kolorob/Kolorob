90d84c015ff95f46c91577e0201729b59a54607e
arafat <mazharul0904055@yahoo.com>
2016-07-12 16:58:01 +0600

    New Table Created as HealthSpecialistTableDetails

---
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/database/Health/HealthSpecialistTableDetails.java
@@ -0,0 +1,7 @@
+package demo.kolorob.kolorobdemoversion.database.Health;
+
+/**
+ * Created by Mazharul.Islam1 on 7/12/2016.
+ */
+public class HealthSpecialistTableDetails {
+}
--- app/src/main/java/demo/kolorob/kolorobdemoversion/model/Health/HealthSpecialistItemDetails.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/model/Health/HealthSpecialistItemDetails.java
@@ -1,79 +1,186 @@
 package demo.kolorob.kolorobdemoversion.model.Health;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import demo.kolorob.kolorobdemoversion.database.DatabaseHelper;
+import demo.kolorob.kolorobdemoversion.database.DatabaseManager;
+import demo.kolorob.kolorobdemoversion.utils.Lg;
+
 /**
- * Created by Mazharul.Islam1 on 7/12/2016.
+ * Created by arafat 12 july 2016.
  */
 
+public class HealthSpecialistTable {
+    private static final String TAG = HealthSpecialistTable.class.getSimpleName();
+    private static final String TABLE_NAME = DatabaseHelper.HEALTH_SPECIALIST_TABLE;
 
-public class HealthSpecialistItemDetails {
-    private String nodeId;
-    private String specialistId;
-    private String specialisttype;
-    private String specialistfees;
-    private String specialistremarks;
-    private int refNum;
-    private String week_fee;
-    private String month_fee;
-    private String report_fee;
-    private String other_fee;
+    private static final String KEY_NODE_ID = "_nodeId"; // 0 -integer
+    private static final String KEY_SPECIALIST_ID = "_specialistId"; // 1 - text
+    private static final String KEY_SPECIALIST_TYPE= "_specialisttype"; // 2 - text*/
+    private static final String KEY_SPECIALIST_FEES = "_specialistfees";
+
+    private static final String KEY_SPECIALIST_REMARKS= " _specialistremarks";
+    private static final String KEY_REF_NUM = "_refNum"; //
+    private static final String KEY_SPECIALIST_TYPE= "_specialisttype"; // 2 - text*/
+    private static final String KEY_SPECIALIST_FEES = "_specialistfees";
+
+    private static final String KEY_SPECIALIST_REMARKS= " _specialistremarks";
+    private static final String KEY_REF_NUM = "_refNum"; //
 
 
-    public HealthSpecialistItemDetails(String nodeId,
-                                       String specialistId,
-                                       String specialisttype,
-                                       String specialistfees,
-                                       String specialistremarks,
-                                       int refNum,
-                                       String week_fee,
-                                       String month_fee,
-                                       String report_fee,
-                                       String other_fee) {
-        this.nodeId = nodeId;
-        this.specialistId = specialistId;
-        this.specialisttype = specialisttype;
-        this.specialistfees = specialistfees;
-        this.specialistremarks = specialistremarks;
-        this.refNum = refNum;
-        this.week_fee=week_fee;
-        this.month_fee=month_fee;
-        this.report_fee=report_fee;
-        this.other_fee=other_fee;
+
+
+    private Context tContext;
+
+
+    public HealthSpecialistTable(Context context) {
+        tContext = context;
+        createTable();
+    }
+
+    private void createTable() {
+        SQLiteDatabase db = openDB();
+
+        String CREATE_TABLE_SQL = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME
+                + "( "
+                + KEY_NODE_ID + "  INTEGER , " // 0 - int
+                + KEY_SPECIALIST_ID  + "  TEXT, "              // 1 - text
+                + KEY_SPECIALIST_TYPE + " TEXT, "
+                + KEY_SPECIALIST_FEES + " TEXT, "
+                + KEY_SPECIALIST_REMARKS + " TEXT, "// 2 - text
+                + KEY_REF_NUM + " INTEGER, PRIMARY KEY(" + KEY_NODE_ID + ", " + KEY_SPECIALIST_ID + "," + KEY_REF_NUM + "))";
+        db.execSQL(CREATE_TABLE_SQL);
+        closeDB();
+    }
+
+    private SQLiteDatabase openDB() {
+        return DatabaseManager.getInstance(tContext).openDatabase();
+    }
+
+    private void closeDB() {
+        DatabaseManager.getInstance(tContext).closeDatabase();
     }
 
 
-    public static HealthSpecialistItemDetails parseHealthSpecialistItem(JSONObject jo,int foreign_key) throws JSONException {
-        String _nodeId=jo.getString("id");
-        String _specialistId= jo.getString("num_doctors");
-        String _specialisttype=jo.getString("specialist_type");
+    public long insertItemHealth(HealthSpecialistItem healthSpecialistItem  ) {
+        return insertItemHealth(
+                healthSpecialistItem.getNodeId(),
+                healthSpecialistItem.getSpecialistId(),
+                healthSpecialistItem.getSpecialisttype(),
+                healthSpecialistItem.getSpecialistfees(),
+                healthSpecialistItem.getSpecialistremarks(),
+                healthSpecialistItem.getRefNum()
+        );
+    }
 
-        String _specialistfees=jo.getString("first_visit_fee");
-        String _specialistremarks=jo.getString("specialist_remarks");
-        int _refNum= foreign_key;
-        String week_fee=jo.getString("week_fee");
-        String month_fee=jo.getString("month_fee");
-        String report_fee=jo.getString("report_fee");
-        String other_fee=jo.getString("other_fee");
+    public long insertItemHealth(String nodeId, String specialistId, String specialisttype,String specialistfees, String specialistremarks, int refNum) {
+        if (isFieldExist(nodeId,specialistId, refNum)) {
+            return updateItem(
+                    nodeId,
+                    specialistId,
+                    specialisttype,
+                    specialistfees,
+                    specialistremarks,
+                    refNum
+            );
+        }
+
+        ContentValues rowValue = new ContentValues();
+        rowValue.put(KEY_NODE_ID , nodeId);
+        rowValue.put(KEY_SPECIALIST_ID,  specialistId);
+        rowValue.put(KEY_SPECIALIST_TYPE,  specialisttype );
+        rowValue.put(KEY_SPECIALIST_FEES , specialistfees);
+        rowValue.put(KEY_SPECIALIST_REMARKS,  specialistremarks );
+        rowValue.put(KEY_REF_NUM   , refNum);
+        SQLiteDatabase db = openDB();
+        long ret = db.insert(TABLE_NAME, null, rowValue);
+        closeDB();
+        return ret;
+    }
+
+    public boolean isFieldExist(String id,String specialistId,int sub_cat_id) {
+        //Lg.d(TAG, "isFieldExist : inside, id=" + id);
+        SQLiteDatabase db = openDB();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+        if (cursor.moveToFirst()) {
+            do {
+                if (id.equals(cursor.getString(0))&&specialistId.equals(cursor.getString(1))&&Integer.parseInt(cursor.getString(5))==sub_cat_id) {
+                    cursor.close();
+                    closeDB();
+                    return true;
+                }
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        closeDB();
+        return false;
+    }
+
+    private long updateItem(String nodeId, String specialistId, String specialisttype,String specialistfees, String specialistremarks, int refNum) {
+        ContentValues rowValue = new ContentValues();
+        rowValue.put(KEY_NODE_ID , nodeId);
+        rowValue.put(KEY_SPECIALIST_ID,  specialistId);
+        rowValue.put(KEY_SPECIALIST_TYPE,  specialisttype );
+        rowValue.put(KEY_SPECIALIST_FEES , specialistfees);
+        rowValue.put(KEY_SPECIALIST_REMARKS,  specialistremarks );
+        rowValue.put(KEY_REF_NUM   , refNum);
+
+
+        SQLiteDatabase db = openDB();
+        long ret = db.update(TABLE_NAME, rowValue, KEY_NODE_ID + " = ? AND"+ KEY_SPECIALIST_ID+ " = ? ",
+                new String[]{nodeId + "",specialistId + ""});
+        closeDB();
+        return ret;
+    }
+
+    public void dropTable() {
+        SQLiteDatabase db = openDB();
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        createTable();
+        Lg.d(TAG, "Table dropped and recreated.");
+        closeDB();
+    }
+    public ArrayList<HealthSpecialistItem> getSpecialistforNode(String nodeId) {
+        ArrayList<HealthSpecialistItem> specialistList = new ArrayList<>();
+        //System.out.println(cat_id+"  "+sub_cat_id);
+        SQLiteDatabase db = openDB();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME +" WHERE "+KEY_NODE_ID+" = '"+nodeId+"'" , null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                //System.out.println("abc="+cursor.getString(4));
+                specialistList.add(cursorToSubCatList(cursor));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        closeDB();
+        return specialistList;
+    }
+
+    private HealthSpecialistItem cursorToSubCatList(Cursor cursor) {
+        String _nodeId=cursor.getString(0);
+        String _specialistId=cursor.getString(1);
+        String  _specialisttype=cursor.getString(2);
+        String _specialistfees=cursor.getString(3);
+
+        String _specialistremarks=cursor.getString(4);
+        int _refNum=cursor.getInt(5);
 
 
 
-        return new HealthSpecialistItemDetails(
+        return new HealthSpecialistItem(
                 _nodeId,
                 _specialistId,
                 _specialisttype,
                 _specialistfees,
                 _specialistremarks,
-                _refNum,
-                week_fee,
-                month_fee,
-                report_fee,
-                other_fee
-
+                _refNum
         );
-
-
-
-
     }
-}
-
