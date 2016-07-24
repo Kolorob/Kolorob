e4aaf6ee530076b69f34b55227952a06f1ba81e6
arafat <mazharul0904055@yahoo.com>
2016-07-24 10:15:07 +0600

    Reactivate list for financial

--- app/src/main/java/demo/kolorob/kolorobdemoversion/database/Financial/FinancialServiceNewTable.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/database/Financial/FinancialServiceNewTable.java
@@ -326,7 +326,7 @@ public class FinancialServiceNewTable {
         places="Mirpur-10";
         SQLiteDatabase db = openDB();
         int i=0;
-        Cursor cursor =db.rawQuery("SELECT * FROM " + DatabaseHelper.SUB_CATEGORY_NEW +  " WHERE _subcatnamebn = '"+subcatnames+"'" ,null);
+        Cursor cursor =db.rawQuery("SELECT * FROM " + DatabaseHelper.SUB_CATEGORY_NEW +  " WHERE _subcatname = '"+subcatnames+"'" ,null);
         if (cursor.moveToFirst()) {
             do {
 
