005b9fbc39872c7232bb671165a8daf50a1fdba7
isratmity01 <mity.crystal@gmail.com>
2016-08-09 11:58:06 +0600

    model changed for financial

--- app/src/main/java/demo/kolorob/kolorobdemoversion/model/FInancial/FinancialNewItem.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/model/FInancial/FinancialNewItem.java
@@ -45,13 +45,14 @@ public class FinancialNewItem  implements Serializable {
 
     String refnumm;
     String rating;
-
+String subref;
     public FinancialNewItem(int finId, String nameen, String namebn, String lat, String lon, String floor, String housename,
                             String houseno, String road, String line, String avenue, String block, String area, String landmark,
                             String postoffice, String policestation, String city, String country, String node_contact,
                             String node_contact2, String node_email, String node_website, String node_facebook,
                             String node_designation, String address, String openingtime, String closetime, String breaktime,
-                            String offday, String registeredwith, String registerednumber, int categoryId, String refnumm,String rating) {
+                            String offday, String registeredwith, String registerednumber, int categoryId, String refnumm,
+                            String rating,String subref) {
         this.finId = finId;
         this.nameen = nameen;
         this.namebn = namebn;
@@ -86,6 +87,7 @@ public class FinancialNewItem  implements Serializable {
         this.categoryId = categoryId;
         this.refnumm = refnumm;
         this.rating=rating;
+        this.subref=subref;
     }
 
     public FinancialNewItem(int finId, String nameen, String namebn) {
@@ -117,6 +119,14 @@ public class FinancialNewItem  implements Serializable {
 
     public String getAddress() {
         return address;
+    }
+
+    public String getSubref() {
+        return subref;
+    }
+
+    public void setSubref(String subref) {
+        this.subref = subref;
     }
 
     public void setAddress(String address) {
@@ -408,11 +418,14 @@ public class FinancialNewItem  implements Serializable {
         String k=jr.toString();
         String _refnumm=k.substring(1,k.length()-1);
         String _rating=jo.getString("rating");
+        JSONArray sref2=jo.getJSONArray("sub_categories");
 
+        String ki=sref2.toString();
+        String _sref=ki.substring(1,ki.length()-1);
         return new FinancialNewItem(_finId,_nameen,_namebn,_lat, _lon,_floor,_housename,_houseno,_road,_line,_avenue,_block,_area,_landmark,_postoffice,_policestation,
                 _city,_country,_node_contact,_node_contact2,_node_email,_node_website,_node_facebook,_node_designation,_address,
                 _opentime,
                 _breaktime,_closetime,_offday,_regwith,
-                _regnum,_catid,_refnumm,_rating);
+                _regnum,_catid,_refnumm,_rating,_sref);
     }
 }
