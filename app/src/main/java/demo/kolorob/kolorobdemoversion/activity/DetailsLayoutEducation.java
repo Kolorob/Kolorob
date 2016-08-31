7ec3dd7a9a0724a560f061483133a3fc8bed2f08
arafat <mazharul0904055@yahoo.com>
2016-08-31 19:46:32 +0600

    Done Comment Section for finance

--- app/src/main/java/demo/kolorob/kolorobdemoversion/activity/DetailsLayoutEducation.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/activity/DetailsLayoutEducation.java
@@ -51,13 +51,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 import demo.kolorob.kolorobdemoversion.R;
+import demo.kolorob.kolorobdemoversion.adapters.Comment_layout_adapter;
 import demo.kolorob.kolorobdemoversion.adapters.DefaultAdapter;
+import demo.kolorob.kolorobdemoversion.database.CommentTable;
 import demo.kolorob.kolorobdemoversion.database.Education.EducationResultDetailsTable;
 import demo.kolorob.kolorobdemoversion.database.Education.EducationTrainingDetailsTable;
 import demo.kolorob.kolorobdemoversion.database.Education.EducationTuitionDetailsTable;
 import demo.kolorob.kolorobdemoversion.fragment.MapFragmentRouteOSM;
 import demo.kolorob.kolorobdemoversion.helpers.Helpes;
 import demo.kolorob.kolorobdemoversion.interfaces.VolleyApiCallback;
+import demo.kolorob.kolorobdemoversion.model.CommentItem;
 import demo.kolorob.kolorobdemoversion.model.Education.EducationNewItem;
 import demo.kolorob.kolorobdemoversion.model.Education.EducationResultItemNew;
 import demo.kolorob.kolorobdemoversion.model.Education.EducationTrainingDetailsItem;
@@ -110,6 +113,8 @@ public class DetailsLayoutEducation extends AppCompatActivity {
     String result_concate = "";
     private CheckBox checkBox;
     EditText feedback_comment;
+    ArrayList<CommentItem> commentItems;
+    ImageView comments;
 ArrayList<String>examname=new ArrayList<>();
 
     @Override
@@ -173,7 +178,63 @@ ArrayList<String>examname=new ArrayList<>();
 
         top_logo = (ImageView) findViewById(R.id.top_logo);
 
+        comments = (ImageView)findViewById(R.id.comments);
+        CommentTable commentTable = new CommentTable(DetailsLayoutEducation.this);
 
+        Log.d("Node Id","======="+educationNewItem.getEduId());
+        commentItems=commentTable.getAllFinancialSubCategoriesInfo(String.valueOf(educationNewItem.getEduId()));
+        int size= commentItems.size();
+        String[] phone = new String[size];
+        String[] date = new String[size];
+        String[] comment = new String[size];
+        int inc=0;
+
+        for (CommentItem commentItem:commentItems)
+        {
+            phone[inc]= commentItem.getService_id();
+            date[inc]=commentItem.getComment();
+            comment[inc]= commentItem.getDate();
+            inc++;
+        }
+
+        final Comment_layout_adapter comment_layout_adapter = new Comment_layout_adapter(this,phone,date,comment);
+
+
+        comments.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater layoutInflater = LayoutInflater.from(DetailsLayoutEducation.this);
+                final View promptView = layoutInflater.inflate(R.layout.comment_popup, null);
+                final Dialog alertDialog = new Dialog(DetailsLayoutEducation.this);
+                alertDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                alertDialog.setContentView(promptView);
+                alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                alertDialog.show();
+
+
+                final TextView textView=(TextView)promptView.findViewById(R.id.header);
+                final ListView listView=(ListView)promptView.findViewById(R.id.comment_list);
+
+                final ImageView close = (ImageView) promptView.findViewById(R.id.closex);
+
+                listView.setAdapter(comment_layout_adapter);
+                textView.setVisibility(View.GONE);
+
+
+                close.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        alertDialog.dismiss();
+                    }
+                });
+
+
+                alertDialog.setCancelable(false);
+
+
+                alertDialog.show();
+            }
+        });
 
 
         distance_left = (ImageView) findViewById(R.id.distance_left);
