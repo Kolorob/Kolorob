e20799b2c0a23a53a901289ec1f1135fe54c8ace
isratmity01 <mity.crystal@gmail.com>
2016-09-25 11:23:27 +0600

    registration page changed

--- app/src/main/java/demo/kolorob/kolorobdemoversion/activity/PhoneRegActivity.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/activity/PhoneRegActivity.java
@@ -1,17 +1,10 @@
 package demo.kolorob.kolorobdemoversion.activity;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -24,7 +17,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import demo.kolorob.kolorobdemoversion.R;
-import demo.kolorob.kolorobdemoversion.model.Job.JobServiceProviderItem;
 import demo.kolorob.kolorobdemoversion.utils.AlertMessage;
 import demo.kolorob.kolorobdemoversion.utils.SharedPreferencesHelper;
 import demo.kolorob.kolorobdemoversion.utils.ToastMessageDisplay;
@@ -33,30 +25,19 @@ import demo.kolorob.kolorobdemoversion.utils.ToastMessageDisplay;
  * Created by arafat on 1/11/2016.
  */
 public class PhoneRegActivity extends Activity {
-    ImageView close,kivabejabejob;
-    TextView close_tv;
-    ImageButton Feedback;
+
     String username="kolorobapp";
     String password="2Jm!4jFe3WgBZKEN";
-    /**
-     * Following components are only for LegalAid
-     * For other categories this components may vary
-     * In that case design the layout for specific category and call them in  setContentView(R.layout.activity_details_info);
-     * */
-    private TextView itemName;
-    private TextView itemAddress;
-    private TextView itemType;
-    private TextView itemContact;
-    private String phoneNumber;
-    private int width;
-    private int height;
 
-    private EditText phone;
-    private EditText name;
-    private TextView fb,openTime,close_Time,breakTIme,jobName,road,block,landmark;
+    private String phoneNumber,uname,emailaddress;
+
+
+    private EditText phone,email,name;
+
+
 
     //TODO Declare object for each subcategory item. Different for each category. Depends on the database table.
-    JobServiceProviderItem jobServiceProviderItem;
+
 
     private Context con;
 
@@ -68,21 +49,28 @@ public class PhoneRegActivity extends Activity {
 
 
         phone  = (EditText)findViewById(R.id.phone_id);
-        phoneNumber=phone.getText().toString();
+        name=(EditText)findViewById(R.id.userid) ;
+        email=(EditText)findViewById(R.id.emailid) ;
 
 
         con = this;
-        DisplayMetrics displayMetrics = this.getResources().getDisplayMetrics();
-        height = displayMetrics.heightPixels;
-        width = displayMetrics.widthPixels;
-
     }
 
     public void submit(View v) {
 
-        phoneNumber = phone.getText().toString();
+        phoneNumber=phone.getText().toString().trim();
+        uname=name.getText().toString().trim();
+        emailaddress=email.getText().toString().trim();
         int size = phoneNumber.length();
+        if( uname.equals("")){
 
+            /**
+             *   You can Toast a message here that the Username is Empty
+             **/
+
+            name.setError( "name is required!" );
+
+        }
         if (size != 11) {
             AlertMessage.showMessage(this, "দুঃখিত আপনার ফোন নম্বরটি সঠিক নয়",
                     "অনুগ্রহ পূর্বক সঠিক ফোন নম্বরটি ইনপুট দিন");
@@ -107,7 +95,7 @@ public class PhoneRegActivity extends Activity {
     {
 
        // http://192.168.43.57/demo/api/customer_reg?phone=01711310912
-        String url = "http://kolorob.net/demo/api/customer_reg?phone="+phone+"&username="+username+"&password="+password+"" ;
+        String url = "http://kolorob.net/demo/api/customer_reg?phone="+phone+"email="+emailaddress+"name="+uname+"&username="+username+"&password="+password+"" ;
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
@@ -188,20 +176,7 @@ public class PhoneRegActivity extends Activity {
 
 
     }
-    private boolean checkPermission(){
-        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE);
-        if (result == PackageManager.PERMISSION_GRANTED){
-
-            return true;
-
-        } else {
-
-            return false;
-
-        }
-    }
-
-
+  
 
     @Override
     public void onBackPressed() {
