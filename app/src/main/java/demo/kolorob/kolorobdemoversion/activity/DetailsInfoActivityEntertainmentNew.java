6a9da363af61b821fe7124d17896989c6a5b3c53
isratmity01 <mity.crystal@gmail.com>
2016-11-02 16:20:48 +0600

    checked internet if activated in entertainment

--- app/src/main/java/demo/kolorob/kolorobdemoversion/activity/DetailsInfoActivityEntertainmentNew.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/activity/DetailsInfoActivityEntertainmentNew.java
@@ -653,9 +653,19 @@ public class DetailsInfoActivityEntertainmentNew extends AppCompatActivity {
                 rb1 = (RadioButton)promptView.findViewById(selected);
                 status = rb1.getText().toString();
                 //  declareRadiobutton();
-                sendReviewToServer();
+               if(AppUtils.isNetConnected(getApplicationContext()))
+               {
+                   sendReviewToServer();
+                   alertDialog.cancel();
+               }
+                else {
+                   ToastMessageDisplay.setText(DetailsInfoActivityEntertainmentNew.this,"দয়া করে ইন্টারনেট চালু করুন।");
+//                    Toast.makeText(this, "আপনার ফোনে ইন্টারনেট সংযোগ নেই। অনুগ্রহপূর্বক ইন্টারনেট সংযোগটি চালু করুন। ...",
+//                            Toast.LENGTH_LONG).show();
+                   ToastMessageDisplay.showText(DetailsInfoActivityEntertainmentNew.this);
+               }
 
-                alertDialog.cancel();
+
 
             }
         });
