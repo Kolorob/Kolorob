34ea525705d8d9e7e3dabc8e0cee9b4980bda7a6
arafat <mazharul0904055@yahoo.com>
2016-11-15 14:38:07 +0600

    Check Internet connection before loading user's bazar post.

--- app/src/main/java/demo/kolorob/kolorobdemoversion/activity/UsersPostActivity.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/activity/UsersPostActivity.java
@@ -1,8 +1,12 @@
 package demo.kolorob.kolorobdemoversion.activity;
 
+import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,184 +19,210 @@ import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import demo.kolorob.kolorobdemoversion.R;
 import demo.kolorob.kolorobdemoversion.adapters.BazarToolAdapter;
 import demo.kolorob.kolorobdemoversion.database.DatabaseManager;
 import demo.kolorob.kolorobdemoversion.interfaces.VolleyApiCallback;
 import demo.kolorob.kolorobdemoversion.model.BazarItem;
+import demo.kolorob.kolorobdemoversion.utils.AlertMessage;
 import demo.kolorob.kolorobdemoversion.utils.AppConstants;
+import demo.kolorob.kolorobdemoversion.utils.AppUtils;
 
 import static demo.kolorob.kolorobdemoversion.parser.VolleyApiParser.getRequest;
 
 public class UsersPostActivity extends AppCompatActivity {
 
+    String user="kolorobapp";
+    String pass="2Jm!4jFe3WgBZKEN";
+    ArrayList<String> bazar_data;
+    ArrayList<BazarItem> allBazar = new ArrayList<BazarItem>();
+    List<String> listDataHeader;
+    HashMap<String, ArrayList<String>> listDataChild;
+    private int bazar_counter=0;
+    ArrayList<ArrayList<String>> myList;
+    ExpandableListView expListView;
+    BazarToolAdapter bazarToolAdapter;
+    private int lastExpandedPosition = -1;
+    ProgressDialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users_post);
 
+        if ((AppUtils.isNetConnected(getApplicationContext()) )&&(ContextCompat.checkSelfPermission(UsersPostActivity.this, Manifest.permission.INTERNET)== PackageManager.PERMISSION_GRANTED ))
+        {
+            dialog = new ProgressDialog(UsersPostActivity.this);
+            dialog.setMessage("দয়া করে অপেক্ষা করুন");
+            dialog.setCancelable(false);
+            dialog.show();
 
-        getRequest(UsersPostActivity.this, "http://kolorob.net/demo/api/getadvsql?username=" + user + "&password=" + pass + " ", new VolleyApiCallback() {
-                    @Override
-                    public void onResponse(int status, String apiContent) {
 
-                        if (status == AppConstants.SUCCESS_CODE) {
-                            //ge the db instance
-                            SQLiteDatabase db = DatabaseManager.getInstance(PlaceDetailsActivityNewLayout.this).openDatabase();
-                            //split into single sql queries
-                            String[] sql = apiContent.split("~");
-                            //run the sqls one by one
-                            for (int i = 0; i<sql.length;i++)
-                            {
-                                Log.d("SQL[i]","%%%%%%"+sql[i]);
-                                db.execSQL(sql[i]);
-                            }
-                            //now reload the data taht has beed saved
-                            //get all data from db
-                            Cursor cursor =  db.rawQuery("select * from custom_advertisement", null);
-                            allBazar = new ArrayList<BazarItem>();
-                            int vf=0;
-                            while (cursor.moveToNext()) {
-                                allBazar.add(new BazarItem(cursor));
-                                vf++;
-                            }
-                            Log.d("vvff","%%%%%%"+vf);
-                            //tester. You may delete this portion
-                            Context context = getApplicationContext();
+            getRequest(UsersPostActivity.this, "http://kolorob.net/demo/api/getadvsql?username=" + user + "&password=" + pass + " ", new VolleyApiCallback() {
+                        @Override
+                        public void onResponse(int status, String apiContent) {
+
+                            if (status == AppConstants.SUCCESS_CODE) {
+                                //ge the db instance
+                                SQLiteDatabase db = DatabaseManager.getInstance(UsersPostActivity.this).openDatabase();
+                                //split into single sql queries
+                                String[] sql = apiContent.split("~");
+                                //run the sqls one by one
+                                for (int i = 0; i<sql.length;i++)
+                                {
+                                    Log.d("SQL[i]","%%%%%%"+sql[i]);
+                                    db.execSQL(sql[i]);
+                                }
+                                //now reload the data taht has beed saved
+                                //get all data from db
+                                Cursor cursor =  db.rawQuery("select * from custom_advertisement", null);
+                                allBazar = new ArrayList<BazarItem>();
+                                int vf=0;
+                                while (cursor.moveToNext()) {
+                                    allBazar.add(new BazarItem(cursor));
+                                    vf++;
+                                }
+                                Log.d("vvff","%%%%%%"+vf);
+                                //tester. You may delete this portion
+                                Context context = getApplicationContext();
 //                            CharSequence text = allBazar.get(0).toString();
 //                            int duration = Toast.LENGTH_SHORT;
 //                            Toast toast = Toast.makeText(context, text, duration);
 //                            toast.show();
-                            //tester ends======
-                        }
+                                //tester ends======
+                            }
 
 
-                        listDataHeader = new ArrayList<String>();
-                        listDataChild = new HashMap<String, ArrayList<String>>();
-                        bazar_data=new ArrayList<String>();
-                        bazar_data.clear();
-                        bazar_counter=0;
-                        myList = new ArrayList<ArrayList<String>>(allBazar.size());
-                        int size= allBazar.size();
-                        for(BazarItem bazarItem: allBazar)
-                        {
+                            listDataHeader = new ArrayList<String>();
+                            listDataChild = new HashMap<String, ArrayList<String>>();
+                            bazar_data=new ArrayList<String>();
+                            bazar_data.clear();
+                            bazar_counter=0;
+                            myList = new ArrayList<ArrayList<String>>(allBazar.size());
+                            int size= allBazar.size();
+                            for(BazarItem bazarItem: allBazar)
+                            {
 
-                            //
+                                //
 
-                            String bazarData= "বিবরন: "+bazarItem.description+"@"+
-                                    bazarItem.price+"@"+
-                                    "তারিখ: "+bazarItem.date+"@"+
+                                String bazarData= "বিবরন: "+bazarItem.description+"@"+
+                                        bazarItem.price+"@"+
+                                        "তারিখ: "+bazarItem.date+"@"+
 
-                                    bazarItem.condition+"@"+
-                                    "এলাকা: "+bazarItem.address+"@"+
-                                    bazarItem.phone+"@"+
-                                    "যোগাযোগ নম্বর: "+bazarItem.contact+"@"+
-                                    "পোস্ট দিয়েছেন: "+bazarItem.contact_person+"@"+ "v"
-                                    ;
-
-
-                            Log.d("Bazar Data","============="+bazarData);
-
-                            String group_data= bazarItem.product_name+"@"+
-                                    bazarItem.type+"@"+"v";
-
-                            bazar_data.add(bazar_counter,bazarData);
+                                        bazarItem.condition+"@"+
+                                        "এলাকা: "+bazarItem.address+"@"+
+                                        bazarItem.phone+"@"+
+                                        "যোগাযোগ নম্বর: "+bazarItem.contact+"@"+
+                                        "পোস্ট দিয়েছেন: "+bazarItem.contact_person+"@"+ "v"
+                                        ;
 
 
+                                Log.d("Bazar Data","============="+bazarData);
 
+                                String group_data= bazarItem.product_name+"@"+
+                                        bazarItem.type+"@"+"v";
 
-                            listDataHeader.add(group_data);
-
-                            // myList.add(bazar_counter,bazar_data);
-                            // myList.get(bazar_counter).add(bazarData);
-                            //             myList.add(bazar_data);
-
-                            Log.d("myList","######"+myList);
+                                bazar_data.add(bazar_counter,bazarData);
 
 
 
 
+                                listDataHeader.add(group_data);
 
-                            bazar_counter++;
+                                // myList.add(bazar_counter,bazar_data);
+                                // myList.get(bazar_counter).add(bazarData);
+                                //             myList.add(bazar_data);
 
-
-                        }
-                        ArrayList<String > temp= new ArrayList<String>();
-
-
-                        for(int k=0;k<bazar_counter;k++)
-                        {
-
-                            myList.add(k,bazar_data);
-                            // myList.get(0).set(k,bazar_data.get(k));
-                            //                      myList.add(k,temp);
-
-                            temp.clear();
-                        }
+                                Log.d("myList","######"+myList);
 
 
 
 
-                        for(int i=0;i<bazar_counter;i++)
-                        {
-                            listDataChild.put(listDataHeader.get(i),myList.get(i));
 
-                        }
-                        dialog.cancel();
-
-                        expListView = (ExpandableListView) findViewById(R.id.bazar_list);
-                        bazarToolAdapter = new BazarToolAdapter(context, listDataHeader, listDataChild);
-                        expListView.setAdapter(bazarToolAdapter);
+                                bazar_counter++;
 
 
-                        ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) expListView
-                                .getLayoutParams();
-                        layoutParams.setMargins(0, 0, 0, buttonHeights);//
+                            }
+                            ArrayList<String > temp= new ArrayList<String>();
 
-                        expListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
 
-                            @Override
-                            public void onGroupExpand(int groupPosition) {
-                                if (lastExpandedPosition != -1
-                                        && groupPosition != lastExpandedPosition) {
-                                    expListView.collapseGroup(lastExpandedPosition);
+                            for(int k=0;k<bazar_counter;k++)
+                            {
+
+                                myList.add(k,bazar_data);
+                                // myList.get(0).set(k,bazar_data.get(k));
+                                //                      myList.add(k,temp);
+
+                                temp.clear();
+                            }
+
+
+
+
+                            for(int i=0;i<bazar_counter;i++)
+                            {
+                                listDataChild.put(listDataHeader.get(i),myList.get(i));
+
+                            }
+                            dialog.cancel();
+
+                            expListView = (ExpandableListView) findViewById(R.id.bazar_list);
+                            bazarToolAdapter = new BazarToolAdapter(UsersPostActivity.this, listDataHeader, listDataChild);
+                            expListView.setAdapter(bazarToolAdapter);
+
+
+//                        ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) expListView
+//                                .getLayoutParams();
+//                        layoutParams.setMargins(0, 0, 0, buttonHeights);//
+
+                            expListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+                                @Override
+                                public void onGroupExpand(int groupPosition) {
+                                    if (lastExpandedPosition != -1
+                                            && groupPosition != lastExpandedPosition) {
+                                        expListView.collapseGroup(lastExpandedPosition);
+                                    }
+                                    lastExpandedPosition = groupPosition;
+
+
                                 }
-                                lastExpandedPosition = groupPosition;
+                            });
+                            expListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+                                @Override
+                                public void onGroupCollapse(int groupPosition) {
 
 
-                            }
-                        });
-                        expListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+                                }
+                            });
+                            expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
 
-                            @Override
-                            public void onGroupCollapse(int groupPosition) {
+                                @Override
+                                public boolean onChildClick(ExpandableListView parent, View v,
+                                                            int groupPosition, int childPosition, long id) {
+                                    // TODO Auto-generated method stub
+                                    expListView.collapseGroup(lastExpandedPosition);
 
-
-                            }
-                        });
-                        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
-
-                            @Override
-                            public boolean onChildClick(ExpandableListView parent, View v,
-                                                        int groupPosition, int childPosition, long id) {
-                                // TODO Auto-generated method stub
-                                expListView.collapseGroup(lastExpandedPosition);
-
-                                return false;
-                            }
-                        });
+                                    return false;
+                                }
+                            });
 
 
-                        Log.d("Button Heights","%%%%%%"+buttonHeights);
-                        SlidingUpPanelLayout slidingUpPanelLayout;
-                        slidingUpPanelLayout=(SlidingUpPanelLayout)findViewById(R.id.sliding_layout);
-                        RelativeLayout.LayoutParams slidingp= (RelativeLayout.LayoutParams) slidingUpPanelLayout.getLayoutParams();
+//                        Log.d("Button Heights","%%%%%%"+buttonHeights);
 
 
+
+                        }
                     }
-                }
-        );
+            );
+        }
+        else {
+            AlertMessage.showMessage(UsersPostActivity.this,"আপনার ফোনে ইন্টারনেট সংযোগ নেই।","অনুগ্রহপূর্বক ইন্টারনেট সংযোগটি চালু করুন। ...");
+
+        }
+
 
 
     }
