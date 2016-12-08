7e0dbb40d2723cc4b946e2c111dbbb0f8bcc25eb
arafat <mazharul0904055@yahoo.com>
2016-12-08 09:41:54 +0600

    New Model Created for WardList and AreaList

---
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/model/AreaItem.java
@@ -0,0 +1,24 @@
+package demo.kolorob.kolorobdemoversion.model;
+
+/**
+ * Created by Mazharul.Islam1 on 12/8/2016.
+ */
+public class AreaItem {
+
+    private String name = "";
+
+    public AreaItem(String name) {
+        super();
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
+
---
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/model/WardItem.java
@@ -0,0 +1,24 @@
+package demo.kolorob.kolorobdemoversion.model;
+
+/**
+ * Created by Mazharul.Islam1 on 12/8/2016.
+ */
+
+import java.util.ArrayList;
+
+
+import java.util.ArrayList;
+
+public class WardItem {
+
+    private String name;
+    private ArrayList<AreaItem> arealist = new ArrayList<AreaItem>();
+
+    public WardItem(String name, ArrayList<AreaItem> arealist) {
+        super();
+        this.name = name;
+        this.arealist = arealist;
+    }
+ ;
+}
+
