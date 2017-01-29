ad38c6ad2708a28118e0c1469b0fbf2827aa197f
isratmity01 <mity.crystal@gmail.com>
2017-01-29 16:19:21 +0600

    model for pharmacies

---
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/model/Health/HealthNewDBModelPharmacy.java
@@ -0,0 +1,64 @@
+package demo.kolorob.kolorobdemoversion.model.Health;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by israt.jahan on 1/29/2017.
+ */
+
+public class HealthNewDBModelPharmacy {
+    int servicecenterid;
+    String docavailability;
+    String speciality;
+    String vaccineavailability;
+
+    public HealthNewDBModelPharmacy(int servicecenterid, String docavailability, String speciality, String vaccineavailability) {
+        this.servicecenterid = servicecenterid;
+        this.docavailability = docavailability;
+        this.speciality = speciality;
+        this.vaccineavailability = vaccineavailability;
+    }
+
+    public int getServicecenterid() {
+        return servicecenterid;
+    }
+
+    public void setServicecenterid(int servicecenterid) {
+        this.servicecenterid = servicecenterid;
+    }
+
+    public String getDocavailability() {
+        return docavailability;
+    }
+
+    public void setDocavailability(String docavailability) {
+        this.docavailability = docavailability;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public String getVaccineavailability() {
+        return vaccineavailability;
+    }
+
+    public void setVaccineavailability(String vaccineavailability) {
+        this.vaccineavailability = vaccineavailability;
+    }
+    public static HealthNewDBModelPharmacy parseHealthNewDBModelPharmacy(JSONObject jo) throws JSONException {
+        int _servicecenterid = jo.getInt("id");
+        String _davailable = jo.getString("doctor_available");
+        String _speciality = jo.getString("speciality");
+
+        String _vaccineavailable = jo.getString("vaccine_available");
+
+        return new HealthNewDBModelPharmacy(_servicecenterid,_davailable,
+                _speciality,_vaccineavailable);
+    }
+}
