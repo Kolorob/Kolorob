c6103a820f5fbc619c0873eb2ea66501c850993f
shamimabrishti <shamima2yasmin@gmail.com>
2017-09-16 15:46:50 +0600

    new rating API for NGO

--- app/src/main/java/demo/kolorob/kolorobdemoversion/activity/DetailsInfoActivityNGO.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/activity/DetailsInfoActivityNGO.java
@@ -59,6 +59,8 @@ import demo.kolorob.kolorobdemoversion.utils.AppUtils;
 import demo.kolorob.kolorobdemoversion.utils.SharedPreferencesHelper;
 import demo.kolorob.kolorobdemoversion.utils.ToastMessageDisplay;
 
+import static demo.kolorob.kolorobdemoversion.R.id.comment;
+
 /**
  * Created by zahidul.islam
  */
@@ -95,7 +97,7 @@ public class DetailsInfoActivityNGO extends AppCompatActivity {
     String status = "", phone_num = "", uname = "";
 
 
-    EditText feedback_comment;
+    //EditText feedback_comment;
     String datevalue, datevaluebn;
 
     @Override
@@ -567,7 +569,7 @@ public class DetailsInfoActivityNGO extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                feedback_comment = (EditText) promptView.findViewById(R.id.feedback_comment);
+                //feedback_comment = (EditText) promptView.findViewById(R.id.feedback_comment);
                 feedRadio = (RadioGroup) promptView.findViewById(R.id.feedRadio);
                 int selected = feedRadio.getCheckedRadioButtonId();
                 rb1 = (RadioButton) promptView.findViewById(selected);
@@ -627,17 +629,17 @@ public class DetailsInfoActivityNGO extends AppCompatActivity {
             rating = 5;
 
 
-        String comment = "", comment2 = "";
-        comment = feedback_comment.getText().toString().trim();
+        /*String comment = "", comment2 = "";
+        comment = feedback_comment.getText().toString().trim();*/
         String uname2 = SharedPreferencesHelper.getUname(DetailsInfoActivityNGO.this);
         uname = uname2.replace(' ', '+');
-        ;
-        try {
+
+        /*try {
             comment2 = URLEncoder.encode(comment.replace(" ", "%20"), "utf-8");
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
-        }
-        String url = "http://kolorob.net/kolorob-new-demo/api/sp_rating/" + ngoServiceProviderItemNew.getNgoid() + "?" + "phone=" + phone_num + "&name=" + uname + "&review=" + comment2 + "&rating=" + rating + "&username=" + username + "&password=" + password + "";
+        }*/
+        String url = "http://kolorob.net/kolorob-new-demo/api/sp_rating2/" + ngoServiceProviderItemNew.getNgoid() + "?" + "phone=" + phone_num + "&name=" + uname + "&rating=" + rating + "&username=" + username + "&password=" + password + "";
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
