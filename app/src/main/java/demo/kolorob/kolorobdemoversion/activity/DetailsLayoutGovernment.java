bfdc23f54f722eda2c4bf7ee2324de20d212059d
shamimabrishti <shamima2yasmin@gmail.com>
2017-09-16 15:57:11 +0600

    Rating API - Government

--- app/src/main/java/demo/kolorob/kolorobdemoversion/activity/DetailsLayoutGovernment.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/activity/DetailsLayoutGovernment.java
@@ -62,6 +62,8 @@ import demo.kolorob.kolorobdemoversion.utils.AppUtils;
 import demo.kolorob.kolorobdemoversion.utils.SharedPreferencesHelper;
 import demo.kolorob.kolorobdemoversion.utils.ToastMessageDisplay;
 
+import static demo.kolorob.kolorobdemoversion.R.id.comment;
+
 /**
  * Created by israt.jahan on 7/17/2016.
  */
@@ -72,7 +74,7 @@ public class DetailsLayoutGovernment extends AppCompatActivity {
 
     int width, height;
     TextView ups_text;
-    EditText feedback_comment;
+    //EditText feedback_comment;
     ArrayList<CommentItem> commentItems;
     ArrayList<SubCategoryItemNew> subCategoryItemNews = new ArrayList<>();
     ImageView comments;
@@ -599,7 +601,7 @@ public class DetailsLayoutGovernment extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                feedback_comment=(EditText)promptView.findViewById(R.id.feedback_comment);
+                //feedback_comment=(EditText)promptView.findViewById(R.id.feedback_comment);
                 feedRadio=(RadioGroup)promptView.findViewById(R.id.feedRadio);
                 int selected = feedRadio.getCheckedRadioButtonId();
                 rb1 = (RadioButton)promptView.findViewById(selected);
@@ -640,16 +642,16 @@ public class DetailsLayoutGovernment extends AppCompatActivity {
         else
             rating= 5;
 
-        String comment="",comment2="";
-        comment=feedback_comment.getText().toString().trim();
+        /*String comment="",comment2="";
+        comment=feedback_comment.getText().toString().trim();*/
         String  uname2 = SharedPreferencesHelper.getUname(DetailsLayoutGovernment.this);
         uname=uname2.replace(' ','+');
-        try {
+        /*try {
             comment2=   URLEncoder.encode(comment.replace(" ", "%20"), "utf-8");
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
-        }
-        String url = "http://kolorob.net/kolorob-new-demo/api/sp_rating/"+governmentNewItem.getGovid()+"?"+"phone=" +phone_num +"&name=" +uname +"&review=" +comment2+ "&rating="+rating+"&username="+username+"&password="+password+"";
+        }*/
+        String url = "http://kolorob.net/kolorob-new-demo/api/sp_rating2/"+governmentNewItem.getGovid()+"?"+"phone=" +phone_num +"&name=" +uname + "&rating="+rating+"&username="+username+"&password="+password+"";
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
