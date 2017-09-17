743e9b25625c0331776ec72d5e20a7221c74b3fa
shamimabrishti <shamima2yasmin@gmail.com>
2017-09-17 15:56:51 +0600

    removed previous format

--- app/src/main/java/demo/kolorob/kolorobdemoversion/activity/DetailsInfoActivityEntertainmentNew.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/activity/DetailsInfoActivityEntertainmentNew.java
@@ -196,32 +196,7 @@ public class DetailsInfoActivityEntertainmentNew extends AppCompatActivity {
         }
         if(entertainmentServiceProviderItemNew.getServicetype().equals(true)) CheckConcate("প্রবেশ মূল্য",  "প্রযোজ্য");
 
-        //CheckConcate("সার্ভিস চার্জ",  "প্রযোজ্য নয়");
-        /*CheckConcate("রাস্তা", English_to_bengali_number_conversion(entertainmentServiceProviderItemNew.getRoad()));
-        CheckConcate("ব্লক", English_to_bengali_number_conversion(entertainmentServiceProviderItemNew.getBlock()));
-        CheckConcate("এলাকা", entertainmentServiceProviderItemNew.getAreabn());
-
-
-        if(entertainmentServiceProviderItemNew.getWard().contains("_")){
-            String[] ward = entertainmentServiceProviderItemNew.getWard().split("_");
-            if(ward[1].equals("dakshinkhan")){
-                CheckConcate("ওয়ার্ড", "দক্ষিণখান");
-            }
-            else{
-                CheckConcate("ওয়ার্ড", English_to_bengali_number_conversion(ward[1]));
-            }
-        }
-        else{
-            CheckConcate("ওয়ার্ড", English_to_bengali_number_conversion(entertainmentServiceProviderItemNew.getWard()));
-        }
-
-
-
-        CheckConcate("পুলিশ স্টেশন", entertainmentServiceProviderItemNew.getPolicestation());
-
-        CheckConcate("বাড়ির নাম্বার", English_to_bengali_number_conversion(entertainmentServiceProviderItemNew.getHouseno()));
-
-        */
+  
 
         CheckConcate("ঠিকানা", concatenateAddress(entertainmentServiceProviderItemNew.getHouseno(), entertainmentServiceProviderItemNew.getRoad(), entertainmentServiceProviderItemNew.getBlock(), entertainmentServiceProviderItemNew.getAreabn(), entertainmentServiceProviderItemNew.getWard(), entertainmentServiceProviderItemNew.getPolicestation()));
         CheckConcate("যোগাযোগ", English_to_bengali_number_conversion(entertainmentServiceProviderItemNew.getNode_contact()));
