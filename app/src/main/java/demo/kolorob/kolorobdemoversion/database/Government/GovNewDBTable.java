82efb912d32ad5cc5ccfe4c1bd9d6392b6ab8045
shamimabrishti <shamima2yasmin@gmail.com>
2017-09-23 00:23:42 +0600

    OOP implementation of Government DB

--- app/src/main/java/demo/kolorob/kolorobdemoversion/database/Government/GovNewDBTable.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/database/Government/GovNewDBTable.java
@@ -5,32 +5,30 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import java.util.ArrayList;
-import demo.kolorob.kolorobdemoversion.database.CommonDBTable;
+import demo.kolorob.kolorobdemoversion.database.BaseDBTable;
 import demo.kolorob.kolorobdemoversion.database.DatabaseHelper;
-import demo.kolorob.kolorobdemoversion.database.DatabaseManager;
 import demo.kolorob.kolorobdemoversion.model.CommonModel;
 import demo.kolorob.kolorobdemoversion.model.Government.GovernmentNewDBModel;
-import demo.kolorob.kolorobdemoversion.utils.Lg;
+
 
 /**
  * Created by israt.jahan on 2/9/2017.
  */
 
-public class GovNewDBTable {
-    private static final String TAG = GovNewDBTable.class.getSimpleName();
+public class GovNewDBTable extends BaseDBTable <GovernmentNewDBModel> {
+
     private static final String TABLE_NAME = DatabaseHelper.GOV_NEW;
     private static final String KEY_IDENTIFIER_ID = "_govid"; // 0 -integer
     private static final String KEY_COMMON_ID = "_commonId";
     private static final String KEY_SERVICE_NAME = "_service_name"; // 1 - text
 
-    private Context tContext;
 
     public GovNewDBTable(Context context) {
         tContext = context;
         createTable();
     }
 
-    private void createTable() {
+    public void createTable() {
         SQLiteDatabase db = openDB();
 
         String CREATE_TABLE_SQL = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME
@@ -43,13 +41,6 @@ public class GovNewDBTable {
         closeDB();
     }
 
-    private SQLiteDatabase openDB() {
-        return DatabaseManager.getInstance(tContext).openDatabase();
-    }
-
-    private void closeDB() {
-        DatabaseManager.getInstance(tContext).closeDatabase();
-    }
 
     public long insertItem(GovernmentNewDBModel governmentNewDBModel) {
         if (!isFieldExist(governmentNewDBModel.getGovId())) {
@@ -93,35 +84,18 @@ public class GovNewDBTable {
     }
 
 
-    public boolean isFieldExist(int id) {
-        //Lg.d(TAG, "isFieldExist : inside, id=" + id);
-        SQLiteDatabase db = openDB();
-        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
-        if (cursor.moveToFirst()) {
-            do {
-                if (id == cursor.getInt(0)) {
-                    cursor.close();
-                    closeDB();
-                    return true;
-                }
-            } while (cursor.moveToNext());
-        }
-        cursor.close();
-        closeDB();
-        return false;
+    public boolean isFieldExist(int id ) {
+        return super.isFieldExist(id, TABLE_NAME);
     }
 
 
 
     public GovernmentNewDBModel getNodeInfo(int node) {
 
-        SQLiteDatabase db = openDB();
+        CommonModel commonModel = getCommonModelFromId(node);
         GovernmentNewDBModel governmentNewDBModel = null;
-        CommonDBTable commonDBTable = new CommonDBTable(tContext);
-        CommonModel commonModel = commonDBTable.getNodeInfo(node);
-
+        SQLiteDatabase db = openDB();
         Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " + KEY_COMMON_ID + " = " + node , null);
-
         if (cursor.moveToFirst()) {
             do {
                 governmentNewDBModel = new GovernmentNewDBModel(cursor.getInt(0), commonModel, cursor.getString(2));
@@ -132,56 +106,26 @@ public class GovNewDBTable {
         return governmentNewDBModel;
     }
 
-    private GovernmentNewDBModel cursorToSubCatList(Cursor cursor) {
+    public GovernmentNewDBModel cursorToSubCatList(Cursor cursor) {
 
         int _govId = cursor.getInt(0);
         int _commonId = cursor.getInt(1);
         String _serviceName = cursor.getString(2);
 
-        CommonDBTable commonDBTable = new CommonDBTable(tContext);
-        CommonModel _commonModel = commonDBTable.getNodeInfo(_commonId);
-
-
+        CommonModel _commonModel = getCommonModelFromId(_commonId);
         return new GovernmentNewDBModel(_govId, _commonModel, _serviceName);
-
     }
 
     public void dropTable() {
-        SQLiteDatabase db = openDB();
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-        createTable();
-        Lg.d(TAG, "Table dropped and recreated.");
-        closeDB();
+       super.dropTable(TABLE_NAME);
     }
 
 
-    public ArrayList <GovernmentNewDBModel> getGovByCommonId(int commonId) {
-
-        ArrayList <GovernmentNewDBModel> govList = new ArrayList<>();
-
-        SQLiteDatabase db = openDB();
-        Cursor cursor = db.rawQuery ("SELECT * FROM " + TABLE_NAME + " WHERE " + KEY_COMMON_ID + " = " + commonId, null);
-
-        if (cursor.moveToFirst()) {
-            do {
-
-                govList.add(cursorToSubCatList(cursor));
-
-            } while (cursor.moveToNext());
-        }
-        cursor.close();
-        closeDB();
-        return govList;
+    public ArrayList <GovernmentNewDBModel> getDetailsByCommonId (int commonId) {
+        return super.getDetailsByCommonId(commonId, TABLE_NAME, KEY_COMMON_ID);
     }
 
-    public void delete(String ward,String area, CommonModel commonModel)
-    {
-        CommonDBTable commonDBTable = new CommonDBTable(tContext);
-        commonDBTable.delete(ward, area);
-        DatabaseHelper databaseHelper = new DatabaseHelper(tContext);
-        SQLiteDatabase database = databaseHelper.getWritableDatabase();
-        database.delete(TABLE_NAME, KEY_COMMON_ID + " = " + commonModel.getId(), null);
-
-        database.close();
+    public void delete(String ward,String area, CommonModel commonModel) {
+        super.delete(ward, area, commonModel, TABLE_NAME, KEY_COMMON_ID);
     }
 }
