5908abf49b6df1b3daa2a5845ba93e1ce80c10f1
shamimabrishti <shamima2yasmin@gmail.com>
2017-10-05 15:56:13 +0600

    Fin extends common model

--- app/src/main/java/demo/kolorob/kolorobdemoversion/model/Financial/FinancialNewDBModel.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/model/Financial/FinancialNewDBModel.java
@@ -12,27 +12,10 @@ import demo.kolorob.kolorobdemoversion.model.CommonModel;
  * Created by israt.jahan on 1/29/2017.
  */
 
-public class FinancialNewDBModel implements Serializable {
+public class FinancialNewDBModel extends CommonModel implements Serializable {
 
-    int finId;
-    CommonModel commonModel;
+
     String finType, serviceType;
-
-    public int getFinId() {
-        return finId;
-    }
-
-    public void setFinId(int finId) {
-        this.finId = finId;
-    }
-
-    public CommonModel getCommonModel() {
-        return commonModel;
-    }
-
-    public void setCommonModel(CommonModel commonModel) {
-        this.commonModel = commonModel;
-    }
 
     public String getFinType() {
         return finType;
@@ -50,23 +33,29 @@ public class FinancialNewDBModel implements Serializable {
         this.serviceType = serviceType;
     }
 
-    public FinancialNewDBModel(int finId, CommonModel commonModel, String finType, String serviceType) {
-        this.finId = finId;
-        this.commonModel = commonModel;
+    public FinancialNewDBModel(int id, String nameEn, String nameBn, String houseNo, String block, String road, String area, String areaBn, String parentArea, String ward, String policeStation, String nodeContact, String nodeEmail, String otherInfo, String openingTime, String closingTime, String offDay, String lat, String lon, int categoryId, String subcat, String refNum, String ratings, String finType, String serviceType) {
+        super(id, nameEn, nameBn, houseNo, block, road, area, areaBn, parentArea, ward, policeStation, nodeContact, nodeEmail, otherInfo, openingTime, closingTime, offDay, lat, lon, categoryId, subcat, refNum, ratings);
         this.finType = finType;
         this.serviceType = serviceType;
     }
 
+    public FinancialNewDBModel(CommonModel cm, String finType, String serviceType){
+        super(cm.getId(), cm.getNameEn(), cm.getNameBn(),
+                cm.getHouseNo(), cm.getBlock(), cm.getRoad(), cm.getArea(), cm.getAreaBn(), cm.getParentArea(), cm.getWard(), cm.getPoliceStation(),
+                cm.getNodeContact(), cm.getNodeEmail(), cm.getOtherInfo(),
+                cm.getOpeningTime(), cm.getClosingTime(), cm.getOffDay(),
+                cm.getLat(), cm.getLon(),
+                cm.getCategoryId(), cm.getSubcat(), cm.getRefNum(), cm.getRatings());
+        this.finType = finType;
+        this.serviceType = serviceType;
 
+    }
 
     public static FinancialNewDBModel parseFinancialNewDBModel (JSONObject jo) throws JSONException {
 
-        CommonModel _commonModel = CommonModel.parseCommonModel(jo);
-        int _finId = jo.getInt("id");
-
+        CommonModel cm = parseCommonModel(jo);
         String _finType = jo.getString("type");
         String _serviceType = jo.getString("service");
-
-        return new FinancialNewDBModel(_finId, _commonModel, _finType, _serviceType);
+        return new FinancialNewDBModel(cm, _finType, _serviceType);
     }
 }
