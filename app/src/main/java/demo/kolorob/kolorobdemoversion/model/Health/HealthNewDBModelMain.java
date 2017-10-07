23584582ae1b5f4adf19a50f7f11079e404ba48c
shamimabrishti <shamima2yasmin@gmail.com>
2017-10-07 11:23:27 +0600

    Health extends Common model

--- app/src/main/java/demo/kolorob/kolorobdemoversion/model/Health/HealthNewDBModelMain.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/model/Health/HealthNewDBModelMain.java
@@ -10,28 +10,10 @@ import demo.kolorob.kolorobdemoversion.model.CommonModel;
  * Created by israt.jahan on 1/29/2017.
  */
 
-public class HealthNewDBModelMain implements Serializable {
+public class HealthNewDBModelMain extends CommonModel implements Serializable {
 
-    int healthId;
-    CommonModel commonModel;
     String instituteType;
 
-
-    public int getHealthId() {
-        return healthId;
-    }
-
-    public void setHealthId(int healthId) {
-        this.healthId = healthId;
-    }
-
-    public CommonModel getCommonModel() {
-        return commonModel;
-    }
-
-    public void setCommonModel(CommonModel commonModel) {
-        this.commonModel = commonModel;
-    }
 
     public String getInstituteType() {
         return instituteType;
@@ -41,18 +23,26 @@ public class HealthNewDBModelMain implements Serializable {
         this.instituteType = instituteType;
     }
 
-    public HealthNewDBModelMain(int healthId, CommonModel commonModel, String instituteType) {
-        this.healthId = healthId;
-        this.commonModel = commonModel;
+    public HealthNewDBModelMain(int id, String nameEn, String nameBn, String houseNo, String block, String road, String area, String areaBn, String parentArea, String ward, String policeStation, String nodeContact, String nodeEmail, String otherInfo, String openingTime, String closingTime, String offDay, String lat, String lon, int categoryId, String subcat, String refNum, String ratings, String instituteType) {
+        super(id, nameEn, nameBn, houseNo, block, road, area, areaBn, parentArea, ward, policeStation, nodeContact, nodeEmail, otherInfo, openingTime, closingTime, offDay, lat, lon, categoryId, subcat, refNum, ratings);
+        this.instituteType = instituteType;
+    }
+
+    public HealthNewDBModelMain(CommonModel cm, String instituteType){
+        super(cm.getId(), cm.getNameEn(), cm.getNameBn(),
+                cm.getHouseNo(), cm.getBlock(), cm.getRoad(), cm.getArea(), cm.getAreaBn(), cm.getParentArea(), cm.getWard(), cm.getPoliceStation(),
+                cm.getNodeContact(), cm.getNodeEmail(), cm.getOtherInfo(),
+                cm.getOpeningTime(), cm.getClosingTime(), cm.getOffDay(),
+                cm.getLat(), cm.getLon(),
+                cm.getCategoryId(), cm.getSubcat(), cm.getRefNum(), cm.getRatings());
         this.instituteType = instituteType;
     }
 
     public static HealthNewDBModelMain parseHealthNewDBModelMain (JSONObject jo) throws JSONException {
 
-        CommonModel _commonModel = CommonModel.parseCommonModel(jo);
-        int _healthId = jo.getInt("id");
+        CommonModel _commonModel = parseCommonModel(jo);
         String _instituteType = jo.getString("institute_type");
 
-        return new HealthNewDBModelMain(_healthId, _commonModel, _instituteType);
+        return new HealthNewDBModelMain(_commonModel, _instituteType);
     }
 }
