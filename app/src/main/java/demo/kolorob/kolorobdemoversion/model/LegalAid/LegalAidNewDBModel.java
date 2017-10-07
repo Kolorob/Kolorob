91a350cbb28109599dcc3a1b1f7f2c76dc38264b
shamimabrishti <shamima2yasmin@gmail.com>
2017-10-07 15:12:36 +0600

    Legal extends common model

--- app/src/main/java/demo/kolorob/kolorobdemoversion/model/LegalAid/LegalAidNewDBModel.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/model/LegalAid/LegalAidNewDBModel.java
@@ -9,27 +9,10 @@ import demo.kolorob.kolorobdemoversion.model.CommonModel;
  * Created by israt.jahan on 1/29/2017.
  */
 
-public class LegalAidNewDBModel implements Serializable {
+public class LegalAidNewDBModel extends CommonModel implements Serializable {
 
-    int legalId;
-    CommonModel commonModel;
     String service;
 
-    public int getLegalId() {
-        return legalId;
-    }
-
-    public void setLegalId(int legalId) {
-        this.legalId = legalId;
-    }
-
-    public CommonModel getCommonModel() {
-        return commonModel;
-    }
-
-    public void setCommonModel(CommonModel commonModel) {
-        this.commonModel = commonModel;
-    }
 
     public String getService() {
         return service;
@@ -39,18 +22,27 @@ public class LegalAidNewDBModel implements Serializable {
         this.service = service;
     }
 
-    public LegalAidNewDBModel(int legalId, CommonModel commonModel, String service) {
-        this.legalId = legalId;
-        this.commonModel = commonModel;
+    public LegalAidNewDBModel(int id, String nameEn, String nameBn, String houseNo, String block, String road, String area, String areaBn, String parentArea, String ward, String policeStation, String nodeContact, String nodeEmail, String otherInfo, String openingTime, String closingTime, String offDay, String lat, String lon, int categoryId, String subcat, String refNum, String ratings, String service) {
+        super(id, nameEn, nameBn, houseNo, block, road, area, areaBn, parentArea, ward, policeStation, nodeContact, nodeEmail, otherInfo, openingTime, closingTime, offDay, lat, lon, categoryId, subcat, refNum, ratings);
         this.service = service;
     }
 
+    public LegalAidNewDBModel(CommonModel cm, String service) {
+        super(cm.getId(), cm.getNameEn(), cm.getNameBn(),
+                cm.getHouseNo(), cm.getBlock(), cm.getRoad(), cm.getArea(), cm.getAreaBn(), cm.getParentArea(), cm.getWard(), cm.getPoliceStation(),
+                cm.getNodeContact(), cm.getNodeEmail(), cm.getOtherInfo(),
+                cm.getOpeningTime(), cm.getClosingTime(), cm.getOffDay(),
+                cm.getLat(), cm.getLon(),
+                cm.getCategoryId(), cm.getSubcat(), cm.getRefNum(), cm.getRatings());
+        this.service = service;
+    }
+
+
     public static LegalAidNewDBModel parseLegalAidNewDBModel (JSONObject jo) throws JSONException {
 
-        CommonModel _commonModel = CommonModel.parseCommonModel(jo);
-        int _healthId = jo.getInt("id");
+        CommonModel _commonModel = parseCommonModel(jo);
         String _service = jo.getString("sevice");
 
-        return new LegalAidNewDBModel(_healthId, _commonModel, _service);
+        return new LegalAidNewDBModel(_commonModel, _service);
     }
 }
