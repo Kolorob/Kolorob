272d6ea41951b343f528b1648fdf0e6a09124cbf
shamimabrishti <shamima2yasmin@gmail.com>
2017-10-11 12:15:41 +0600

    Common properties of categories have been implemented in common DB table

--- app/src/main/java/demo/kolorob/kolorobdemoversion/database/CommonDBTable.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/database/CommonDBTable.java
@@ -4,75 +4,68 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
-
 import java.util.ArrayList;
-
-import demo.kolorob.kolorobdemoversion.model.CityCorporation;
 import demo.kolorob.kolorobdemoversion.model.CommonModel;
+
 
 /**
  * Created by shamima.yasmin on 9/21/2017.
  */
 
-public class CommonDBTable extends BaseDBTable <CommonModel> {
 
-    //  DB configuration
-
-    private static final String TAG = CommonDBTable.class.getSimpleName();
-    private static final String TABLE_NAME = DatabaseHelper.COMMON_DB;
+public abstract class CommonDBTable <ModelType extends CommonModel> extends BaseDBTable <ModelType> {
 
     //  name and id
 
-    private static final String KEY_IDENTIFIER_ID = "_id"; // 0 -integer
-    private static final String KEY_NAME_ENG = "_nameen"; //
-    private static final String KEY_NAME_BAN = "_namebn"; //
+
+    private static final String KEY_NAME_ENG = "name_en";
+    private static final String KEY_NAME_BAN = "name_bn";
 
     //  contact info
 
-    private static final String KEY_HOUSE_NO = "_houseno"; //
-    private static final String KEY_BLOCK = "_block"; //
-    private static final String KEY_ROAD = "_road"; //
-    private static final String KEY_AREA = "_area"; //
-    private static final String KEY_AREABN = "_areabn"; //
-    private static final String KEY_PARENT_AREA = "_parentarea";//
-    private static final String KEY_WARD = "_ward";
-    private static final String KEY_POLICE_STATION = "_policestation"; //
-    private static final String KEY_CONTACT_NO = "_node_contact"; //
-    private static final String KEY_EMAIL_ADDRESS = "_node_email"; //
-    private static final String KEY_OTHER_INFO = "_other"; //
+    private static final String KEY_HOUSE_NO = "house_no";
+    private static final String KEY_BLOCK = "block";
+    private static final String KEY_ROAD = "road";
+    private static final String KEY_AREA = "area";
+    private static final String KEY_AREABN = "area_bn";
+    private static final String KEY_PARENT_AREA = "parent_area";
+    private static final String KEY_WARD = "ward";
+    private static final String KEY_POLICE_STATION = "police_station";
+    private static final String KEY_CONTACT_NO = "node_contact";
+    private static final String KEY_EMAIL_ADDRESS = "node_email";
+    private static final String KEY_OTHER_INFO = "other_info";
 
     //  timing info
 
-    private static final String KEY_OPENTIME = "_opentime"; //
-    private static final String KEY_CLOSETIME = "_closetime"; //
-    private static final String KEY_OFF_DAY = "_offday";
+    private static final String KEY_OPENTIME = "opening_time"; //
+    private static final String KEY_CLOSETIME = "closing_time"; //
+    private static final String KEY_OFF_DAY = "off_day";
 
     //  mapping info
 
-    private static final String KEY_LATITUDE = "_lat"; //
-    private static final String KEY_LONGITUDE = "_lon"; //
+    private static final String KEY_LATITUDE = "lat"; //
+    private static final String KEY_LONGITUDE = "lon"; //
 
     //  Other common details
 
-    private static final String KEY_CATEGORY_ID = "_catid";
-    private static final String KEY_SUBCATEGORY_ID = "_sref";
-    private static final String KEY_REFERENCE = "_refnumm";
-    private static final String KEY_RATINGS = "_rating";
+    private static final String KEY_CATEGORY_ID = "category_id";
+    private static final String KEY_SUBCATEGORY_ID = "subcat_id";
+    private static final String KEY_REFERENCE = "references";
+    private static final String KEY_RATINGS = "rating";
 
+
+    public abstract void insertItem(ContentValues rowValue, ModelType model);
+    public abstract void updateItem(ContentValues rowValue, ModelType model);
 
 
     public CommonDBTable(Context context) {
-        tContext = context;
-        createTable();
+        super(context);
     }
 
-    public void createTable() {
-        SQLiteDatabase db = openDB();
-
+    protected String createTableQuery(String TABLE_NAME){
         String CREATE_TABLE_SQL = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME
                 + "( "
-                + KEY_IDENTIFIER_ID + "  INTEGER , "
+                + KEY_IDENTIFIER_ID + " INTEGER PRIMARY KEY, "
                 + KEY_NAME_ENG + "  TEXT, "
                 + KEY_NAME_BAN + " TEXT, "
 
@@ -98,50 +91,43 @@ public class CommonDBTable extends BaseDBTable <CommonModel> {
                 + KEY_CATEGORY_ID + " INTEGER, "
                 + KEY_SUBCATEGORY_ID + " TEXT, "
                 + KEY_REFERENCE+ " TEXT, "
-                + KEY_RATINGS + " TEXT, PRIMARY KEY(" + KEY_IDENTIFIER_ID + "))";
+                + KEY_RATINGS + " TEXT, ";
 
-        db.execSQL(CREATE_TABLE_SQL);
-        closeDB();
+
+        return CREATE_TABLE_SQL;
     }
 
 
-    public long insertItem(CommonModel commonModel) {
-        if (!isFieldExist(commonModel.getId())) {
-            return insertItem(commonModel.getId(), commonModel.getNameEn(), commonModel.getNameBn(),
-                commonModel.getHouseNo(), commonModel.getBlock(), commonModel.getRoad(), commonModel.getArea(), commonModel.getAreaBn(), commonModel.getParentArea(), commonModel.getWard(), commonModel.getPoliceStation(),
-                commonModel.getNodeContact(), commonModel.getNodeEmail(), commonModel.getOtherInfo(),
-                commonModel.getOpeningTime(), commonModel.getClosingTime(), commonModel.getOffDay(),
-                commonModel.getLat(), commonModel.getLon(),
-                commonModel.getCategoryId(), commonModel.getSubcat(), commonModel.getRefNum(), commonModel.getRatings()
-            );
+    public long insertItem(ModelType model, String TABLE_NAME) {
+        if (!isFieldExist(model.getId())) {
+            return insertItem(TABLE_NAME, model, model.getId(), model.getNameEn(), model.getNameBn(),
+                    model.getHouseNo(), model.getBlock(), model.getRoad(), model.getArea(), model.getAreaBn(), model.getParentArea(), model.getWard(), model.getPoliceStation(), model.getNodeContact(), model.getNodeEmail(), model.getOtherInfo(),
+                    model.getOpeningTime(), model.getClosingTime(), model.getOffDay(),
+                    model.getLat(), model.getLon(),
+                    model.getCategoryId(), model.getSubcat(), model.getRefNum(), model.getRatings());
         }
-        else
-            return updateItem(commonModel.getId(), commonModel.getNameEn(), commonModel.getNameBn(),
-                commonModel.getHouseNo(), commonModel.getBlock(), commonModel.getRoad(), commonModel.getArea(), commonModel.getAreaBn(), commonModel.getParentArea(), commonModel.getWard(), commonModel.getPoliceStation(),
-                commonModel.getNodeContact(), commonModel.getNodeEmail(), commonModel.getOtherInfo(),
-                commonModel.getOpeningTime(), commonModel.getClosingTime(), commonModel.getOffDay(),
-                commonModel.getLat(), commonModel.getLon(),
-                commonModel.getCategoryId(), commonModel.getSubcat(), commonModel.getRefNum(), commonModel.getRatings()
-                );
-
+        else {
+            return updateItem(TABLE_NAME, model, model.getId(), model.getNameEn(), model.getNameBn(),
+                    model.getHouseNo(), model.getBlock(), model.getRoad(), model.getArea(), model.getAreaBn(), model.getParentArea(), model.getWard(), model.getPoliceStation(), model.getNodeContact(), model.getNodeEmail(), model.getOtherInfo(),
+                    model.getOpeningTime(), model.getClosingTime(), model.getOffDay(),
+                    model.getLat(), model.getLon(),
+                    model.getCategoryId(), model.getSubcat(), model.getRefNum(), model.getRatings());
+        }
     }
 
 
-    public long insertItem(int id, String nameEn, String nameBn,
-                           String houseNo, String block, String road, String area, String areaBn, String parentArea, String ward, String policeStation, String nodeContact, String nodeEmail, String otherInfo,
-                           String openingTime, String closingTime, String offDay,
-                           String lat, String lon,
-                           int catId, String subCat, String refNum, String ratings)
-    {
-        if (isFieldExist(id)) {
-            return updateItem(
-                    id, nameEn, nameBn,
-                    houseNo, block, road, area, areaBn, parentArea, ward, policeStation, nodeContact, nodeEmail, otherInfo,
-                    openingTime, closingTime, offDay,
-                    lat, lon,
-                    catId, subCat, refNum, ratings
-            );
-        }
+    public long updateItem(ModelType model, String TABLE_NAME){
+        return updateItem(TABLE_NAME, model, model.getId(), model.getNameEn(), model.getNameBn(),
+                model.getHouseNo(), model.getBlock(), model.getRoad(), model.getArea(), model.getAreaBn(), model.getParentArea(), model.getWard(), model.getPoliceStation(), model.getNodeContact(), model.getNodeEmail(), model.getOtherInfo(),
+                model.getOpeningTime(), model.getClosingTime(), model.getOffDay(),
+                model.getLat(), model.getLon(),
+                model.getCategoryId(), model.getSubcat(), model.getRefNum(), model.getRatings());
+    }
+
+
+
+    public long insertItem(String TABLE_NAME, ModelType model, int id, String nameEn, String nameBn, String houseNo, String block, String road, String area, String areaBn, String parentArea, String ward, String policeStation, String nodeContact, String nodeEmail, String otherInfo, String openingTime, String closingTime, String offDay, String lat, String lon, int categoryId, String subcat, String refNum, String ratings) {
+
         ContentValues rowValue = new ContentValues();
 
         rowValue.put(KEY_IDENTIFIER_ID, id);
@@ -167,10 +153,12 @@ public class CommonDBTable extends BaseDBTable <CommonModel> {
         rowValue.put(KEY_LATITUDE, lat);
         rowValue.put(KEY_LONGITUDE, lon);
 
-        rowValue.put(KEY_CATEGORY_ID, catId);
-        rowValue.put(KEY_SUBCATEGORY_ID, subCat);
+        rowValue.put(KEY_CATEGORY_ID, categoryId);
+        rowValue.put(KEY_SUBCATEGORY_ID, subcat);
         rowValue.put(KEY_REFERENCE, refNum);
         rowValue.put(KEY_RATINGS, ratings);
+
+        insertItem(rowValue, model);
 
         SQLiteDatabase db = openDB();
         long insertedId = db.insert(TABLE_NAME, null, rowValue);
@@ -179,13 +167,9 @@ public class CommonDBTable extends BaseDBTable <CommonModel> {
         closeDB();
         return insertedId;
     }
-    private long updateItem(
-            int id, String nameEn, String nameBn,
-            String houseNo, String block, String road, String area, String areaBn, String parentArea, String ward, String policeStation, String nodeContact, String nodeEmail, String otherInfo,
-            String openingTime, String closingTime, String offDay,
-            String lat, String lon,
-            int catId, String subCat, String refNum, String ratings
-    ) {
+
+
+    private long updateItem(String TABLE_NAME, ModelType model, int id, String nameEn, String nameBn, String houseNo, String block, String road, String area, String areaBn, String parentArea, String ward, String policeStation, String nodeContact, String nodeEmail, String otherInfo, String openingTime, String closingTime, String offDay, String lat, String lon, int categoryId, String subcat, String refNum, String ratings) {
 
         ContentValues rowValue = new ContentValues();
 
@@ -212,23 +196,44 @@ public class CommonDBTable extends BaseDBTable <CommonModel> {
         rowValue.put(KEY_LATITUDE, lat);
         rowValue.put(KEY_LONGITUDE, lon);
 
-        rowValue.put(KEY_CATEGORY_ID, catId);
-        rowValue.put(KEY_SUBCATEGORY_ID, subCat);
+        rowValue.put(KEY_CATEGORY_ID, categoryId);
+        rowValue.put(KEY_SUBCATEGORY_ID, subcat);
         rowValue.put(KEY_REFERENCE, refNum);
         rowValue.put(KEY_RATINGS, ratings);
+
+        updateItem(rowValue, model);
 
 
         SQLiteDatabase db = openDB();
         long updatedId = db.update(TABLE_NAME, rowValue, KEY_IDENTIFIER_ID + " = ?",
                 new String[]{id + ""});
-
         closeDB();
-
         return updatedId;
     }
 
-    public void delete(String ward,String area)
-    {
+
+
+    public ArrayList <ModelType> getByAreaCategory(String ward, String area, int category, String TABLE_NAME) {
+
+        ArrayList <ModelType> list = new ArrayList<>();
+
+        SQLiteDatabase db = openDB();
+        Cursor cursor = db.rawQuery ("SELECT * FROM " +  TABLE_NAME + " WHERE " + KEY_CATEGORY_ID + " = " + category + " AND " + KEY_WARD + " = '"+ ward + "' AND " + "(" + KEY_AREA + "  = '" + area  + "' OR "  + KEY_PARENT_AREA + "  =  '" + area + "')", null);
+
+        if (cursor.moveToFirst()) {
+            do {
+
+                list.add(cursorToModel(cursor));
+
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        closeDB();
+        return list;
+    }
+
+
+    public void delete(String ward, String area, String TABLE_NAME) {
         DatabaseHelper databaseHelper = new DatabaseHelper(CommonDBTable.this.tContext);
         SQLiteDatabase database = databaseHelper.getWritableDatabase();
         database.delete(TABLE_NAME, KEY_WARD + " = '" + ward + "' AND " + "(" + KEY_AREA + " = '" + area + "')" + " OR " + "(" + KEY_PARENT_AREA + "  =  '" + area + "')", null);
@@ -236,20 +241,20 @@ public class CommonDBTable extends BaseDBTable <CommonModel> {
         database.close();
     }
 
-    public void delete(int id){
-        super.delete(id, TABLE_NAME, KEY_IDENTIFIER_ID);
+    public void delete(int id, String TABLE_NAME){
+        super.delete(id, TABLE_NAME);
     }
 
-    public boolean isFieldExist(int id) {
-       return super.isFieldExist(id, TABLE_NAME, KEY_IDENTIFIER_ID);
+    public boolean isFieldExist(int id, String TABLE_NAME) {
+       return super.isFieldExist(id, TABLE_NAME);
     }
 
-    public ArrayList <CommonModel> getAllCommonByArea(String ward, String place) {
+    public ArrayList <ModelType> getByArea(String ward, String area, String TABLE_NAME) {
 
-        ArrayList <CommonModel> list = new ArrayList<>();
+        ArrayList <ModelType> list = new ArrayList<>();
 
         SQLiteDatabase db = openDB();
-        Cursor cursor = db.rawQuery ("SELECT * FROM " +  TABLE_NAME + " WHERE " + KEY_WARD + " = '"+ ward + "' AND " + "(" + KEY_AREA + "  = '" + place + "')" + " OR " + "(" + KEY_PARENT_AREA + "  =  '" + place + "')", null);
+        Cursor cursor = db.rawQuery ("SELECT * FROM " +  TABLE_NAME + " WHERE " + KEY_WARD + " = '"+ ward + "' AND " + "(" + KEY_AREA + "  = '" + area + "')" + " OR " + "(" + KEY_PARENT_AREA + "  =  '" + area + "')", null);
 
         if (cursor.moveToFirst()) {
             do {
@@ -263,45 +268,23 @@ public class CommonDBTable extends BaseDBTable <CommonModel> {
         return list;
     }
 
-    public ArrayList <CommonModel> getAllCommonByAreaCategory(String ward, String place, int category) {
 
-        ArrayList <CommonModel> list = new ArrayList<>();
 
-        SQLiteDatabase db = openDB();
-        Cursor cursor = db.rawQuery ("SELECT * FROM " +  TABLE_NAME + " WHERE " + KEY_CATEGORY_ID + " = " + category + " AND " + KEY_WARD + " = '"+ ward + "' AND " + "(" + KEY_AREA + "  = '" + place  + "' OR "  + KEY_PARENT_AREA + "  =  '" + place + "')", null);
-
-        if (cursor.moveToFirst()) {
-            do {
-
-                list.add(cursorToModel(cursor));
-
-            } while (cursor.moveToNext());
-        }
-        cursor.close();
-        closeDB();
-        return list;
+    public ArrayList <ModelType> getDataListFromId(int id, String TABLE_NAME){
+        return super.getDataListFromId(id, TABLE_NAME);
     }
 
-    public ArrayList <CommonModel> getDataListFromId(int id){
-        return super.getDataListFromId(id, TABLE_NAME, KEY_IDENTIFIER_ID);
-    }
-
-    public ArrayList <CommonModel> getAllData(){
+    public ArrayList <ModelType> getAllData(String TABLE_NAME){
         return super.getAllData(TABLE_NAME);
     }
 
-    public CommonModel getDataFromId(int id){
-        return super.getDataFromId(id, TABLE_NAME, KEY_IDENTIFIER_ID);
-    }
 
+    public ModelType getNodeInfo(int node, String TABLE_NAME) {
 
-    public CommonModel getNodeInfo(int Node) {
-
-        Log.e(" ", "GetNodeInfo common " + Node);
         SQLiteDatabase db = openDB();
         CommonModel commonModel = null;
 
-        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " + KEY_IDENTIFIER_ID + " = " + Node , null);
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " + KEY_IDENTIFIER_ID + " = " + node , null);
 
         if (cursor.moveToFirst()) {
             do {
@@ -314,11 +297,11 @@ public class CommonDBTable extends BaseDBTable <CommonModel> {
         }
         cursor.close();
         closeDB();
-        return commonModel;
+        return (ModelType) commonModel;
     }
 
 
-    public CommonModel cursorToModel(Cursor cursor) {
+    public ModelType cursorToModel(Cursor cursor) {
 
         int _id = cursor.getInt(0);
         String _nameEn = cursor.getString(1);
@@ -349,7 +332,7 @@ public class CommonDBTable extends BaseDBTable <CommonModel> {
         String _ratings = cursor.getString(22);
 
 
-        return new CommonModel(_id, _nameEn, _nameBn,
+        return (ModelType) new CommonModel(_id, _nameEn, _nameBn,
                 _houseNo, _block, _road, _area, _areaBn, _parentArea, _ward, _policeStation, _nodeContact, _nodeEmail, _otherInfo,
                 _openingTime, _closingTime, _offDay,
                 _lat, _lon,
@@ -357,11 +340,11 @@ public class CommonDBTable extends BaseDBTable <CommonModel> {
 
     }
 
-    public CommonModel cursorToModel(Cursor cursor, CommonModel commonModel){
+    public ModelType cursorToModel(Cursor cursor, CommonModel commonModel){
         return null;
     }
 
-    public void dropTable() {
+    public void dropTable(String TABLE_NAME) {
         super.dropTable(TABLE_NAME);
     }
 }
