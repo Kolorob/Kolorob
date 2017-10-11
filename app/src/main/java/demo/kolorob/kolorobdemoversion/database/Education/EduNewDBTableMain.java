8296092b879263e802833562aebf07d7bf710c34
shamimabrishti <shamima2yasmin@gmail.com>
2017-10-11 14:53:17 +0600

    Edu table implements common DB methods

--- app/src/main/java/demo/kolorob/kolorobdemoversion/database/Education/EduNewDBTableMain.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/database/Education/EduNewDBTableMain.java
@@ -5,162 +5,81 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import java.util.ArrayList;
-import demo.kolorob.kolorobdemoversion.database.BaseDBTable;
+import demo.kolorob.kolorobdemoversion.database.CommonDBTable;
 import demo.kolorob.kolorobdemoversion.database.DatabaseHelper;
 import demo.kolorob.kolorobdemoversion.model.CommonModel;
 import demo.kolorob.kolorobdemoversion.model.EduNewDB.EduNewModel;
+
 
 /**
  * Created by israt.jahan on 2/9/2017.
  */
 
 
-public class EduNewDBTableMain extends BaseDBTable <EduNewModel> {
+public class EduNewDBTableMain extends CommonDBTable<EduNewModel> {
 
     private static final String TABLE_NAME = DatabaseHelper.EDU_NEW_DB_MAIN;
-    private static final String KEY_IDENTIFIER_ID = "_eduId"; // 0 -integer
-    private static final String KEY_COMMON_ID = "_commonId";
-    private static final String KEY_EDU_TYPE = "_eduType"; // 1 - text
-    private static final String KEY_SHIFT = "_shift"; // 1 - text
-    private static final String KEY_STUDENT_NO = "_studentNo"; // 1 - text
-    private static final String KEY_TEACHER_NO = "_teachersNo"; // 1 - text
-    private static final String KEY_AVG_STD = "_avgStdPerClass"; // 1 - text
-    private static final String KEY_FACILITY = "_facility"; // 1 - text
+
+    private static final String KEY_EDU_TYPE = "edu_type";
+    private static final String KEY_SHIFT = "shift";
+    private static final String KEY_STUDENT_NO = "student_No";
+    private static final String KEY_TEACHER_NO = "teachers_no";
+    private static final String KEY_AVG_STD = "avg_std_per_class";
+    private static final String KEY_FACILITY = "facility";
 
 
     public EduNewDBTableMain(Context context) {
-        tContext = context;
-        createTable();
+        super(context);
     }
 
     public void createTable() {
+
         SQLiteDatabase db = openDB();
-
-        String CREATE_TABLE_SQL = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME
-                + "( "
-                + KEY_IDENTIFIER_ID + "  INTEGER , " // 0 - int
-                + KEY_COMMON_ID + "  INTEGER, "              // 1 - text
-
-                + KEY_EDU_TYPE + " TEXT, "// 2 -
-                + KEY_SHIFT + " TEXT, "// 2 - text
-                + KEY_STUDENT_NO + " TEXT, "// 2 - text
-                + KEY_TEACHER_NO + " TEXT, "// 2 - text
-                + KEY_AVG_STD + " TEXT, "// 2 - text
-                + KEY_FACILITY + " TEXT, PRIMARY KEY(" + KEY_IDENTIFIER_ID + "))";
-
+        String CREATE_TABLE_SQL = createTableQuery(TABLE_NAME);
+        CREATE_TABLE_SQL += KEY_EDU_TYPE + " TEXT, "
+                + KEY_SHIFT + " TEXT, "
+                + KEY_STUDENT_NO + " TEXT, "
+                + KEY_TEACHER_NO + " TEXT, "
+                + KEY_AVG_STD + " TEXT, "
+                + KEY_FACILITY + " TEXT )";
         db.execSQL(CREATE_TABLE_SQL);
         closeDB();
     }
 
 
     public long insertItem(EduNewModel eduNewModel) {
-        if (!isFieldExist(eduNewModel.getEduId()))
-        {
-            return insertItem(eduNewModel.getEduId(), eduNewModel.getCommonModel().getId(),
-                    eduNewModel.getEducationType(), eduNewModel.getShift(),
-                    eduNewModel.getStudentNo(), eduNewModel.getTeachersNo(),
-                    eduNewModel.getAverageStudentPerClass(), eduNewModel.getFacility());
-        }
-        else return updateItem(eduNewModel.getEduId(), eduNewModel.getCommonModel().getId(),
-                eduNewModel.getEducationType(), eduNewModel.getShift(),
-                eduNewModel.getStudentNo(), eduNewModel.getTeachersNo(),
-                eduNewModel.getAverageStudentPerClass(), eduNewModel.getFacility());
-
+        return super.insertItem(eduNewModel, TABLE_NAME);
     }
 
 
-    public long insertItem(int eduId, int commonId, String educationType, String shift,
-                           String studentNo, String teacherNo, String avgStudentPerClass, String facility
-    ) {
-        if (isFieldExist(eduId)) {
-            return updateItem(eduId, commonId, educationType, shift, studentNo, teacherNo, avgStudentPerClass, facility);
-        }
-
-        ContentValues rowValue = new ContentValues();
-
-        rowValue.put(KEY_IDENTIFIER_ID, eduId);
-        rowValue.put(KEY_COMMON_ID, commonId);
-        rowValue.put(KEY_EDU_TYPE, educationType);
-        rowValue.put(KEY_SHIFT, shift);
-        rowValue.put(KEY_STUDENT_NO, studentNo);
-        rowValue.put(KEY_TEACHER_NO, teacherNo);
-        rowValue.put(KEY_AVG_STD, avgStudentPerClass);
-        rowValue.put(KEY_FACILITY, facility);
-
-        SQLiteDatabase db = openDB();
-        long insertedId = db.insert(TABLE_NAME, null, rowValue);
-
-
-        closeDB();
-        return insertedId;
-    }
-    private long updateItem(
-            int eduId, int commonId, String educationType, String shift,
-            String studentNo, String teacherNo, String avgStudentPerClass, String facility) {
-
-        ContentValues rowValue = new ContentValues();
-
-        rowValue.put(KEY_IDENTIFIER_ID, eduId);
-        rowValue.put(KEY_COMMON_ID, commonId);
-        rowValue.put(KEY_EDU_TYPE, educationType);
-        rowValue.put(KEY_SHIFT, shift);
-        rowValue.put(KEY_STUDENT_NO, studentNo);
-        rowValue.put(KEY_TEACHER_NO, teacherNo);
-        rowValue.put(KEY_AVG_STD, avgStudentPerClass);
-        rowValue.put(KEY_FACILITY, facility);
-
-        SQLiteDatabase db = openDB();
-        long updatedId = db.update(TABLE_NAME, rowValue, KEY_IDENTIFIER_ID + " = ?",
-                new String[]{eduId + ""});
-        closeDB();
-        return updatedId;
+    public void insertItem(ContentValues rowValue, EduNewModel eduNewModel) {
+        rowValue.put(KEY_EDU_TYPE, eduNewModel.getEducationType());
+        rowValue.put(KEY_SHIFT, eduNewModel.getShift());
+        rowValue.put(KEY_STUDENT_NO, eduNewModel.getStudentNo());
+        rowValue.put(KEY_TEACHER_NO, eduNewModel.getTeachersNo());
+        rowValue.put(KEY_AVG_STD, eduNewModel.getAverageStudentPerClass());
+        rowValue.put(KEY_FACILITY, eduNewModel.getFacility());
     }
 
 
-    public boolean isFieldExist(int id) {
-        return super.isFieldExist(id, TABLE_NAME, KEY_IDENTIFIER_ID);
+    public long updateItem(EduNewModel eduNewModel) {
+        return super.updateItem(eduNewModel, TABLE_NAME);
     }
 
 
-    public EduNewModel getDetailsByCommonId(int commonId) {     // getAllEducationSubCategoriesInfo
-        return super.getDetailsByCommonId(commonId, TABLE_NAME, KEY_COMMON_ID);
-    }
-
-    public EduNewModel getDetailsByCommonModel(CommonModel commonModel){
-        return super.getDetailsByCommonModel(commonModel, TABLE_NAME, KEY_COMMON_ID);
+    public void updateItem(ContentValues rowValue, EduNewModel eduNewModel){
+        insertItem(rowValue, eduNewModel);
     }
 
 
-    public void delete(int id){
-        super.delete(id, TABLE_NAME, KEY_IDENTIFIER_ID);
+    public boolean isFieldExist(int id ) {
+        return super.isFieldExist(id, TABLE_NAME);
     }
 
-    public EduNewModel getNodeInfo(int node) {
-
-        CommonModel commonModel = getCommonModelFromId(node);
-        EduNewModel eduNewModel = null;
-        SQLiteDatabase db = openDB();
-
-        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " + KEY_COMMON_ID + " = " + node , null);
-
-        if (cursor.moveToFirst()) {
-            do {
-                eduNewModel = new EduNewModel(cursor.getInt(0), commonModel, cursor.getString(2),
-                        cursor.getString(3), cursor.getString(4), cursor.getString(5), cursor.getString(6), cursor.getString(7));
-            } while (cursor.moveToNext());
-        }
-        cursor.close();
-        closeDB();
-        return eduNewModel;
+    public ArrayList <EduNewModel> getDataListFromId(int node){
+        return super.getDataListFromId(node, TABLE_NAME);
     }
 
-    public ArrayList <EduNewModel> getDataListFromId(int nodeId) {
-        return super.getDataListFromId(nodeId, TABLE_NAME, KEY_IDENTIFIER_ID);
-    }
-
-    public EduNewModel getDataFromId(int id){
-        return super.getDataFromId(id, TABLE_NAME, KEY_IDENTIFIER_ID);
-    }
 
     public ArrayList <EduNewModel> getAllData(){
         return super.getAllData(TABLE_NAME);
@@ -169,38 +88,28 @@ public class EduNewDBTableMain extends BaseDBTable <EduNewModel> {
 
     public EduNewModel cursorToModel(Cursor cursor) {
 
-        int _eduId = cursor.getInt(0);
-        int _commonId = cursor.getInt(1);
+        CommonModel _commonModel = super.cursorToModel(cursor);
+        String _edu_type = cursor.getString(23);
+        String _shift = cursor.getString(24);
+        String _student_no = cursor.getString(25);
+        String _teachers_no = cursor.getString(26);
+        String _avg_std_per_class = cursor.getString(27);
+        String _facility = cursor.getString(28);
 
-        String _eduType = cursor.getString(2);
-        String _shift = cursor.getString(3);
-        String _studentNo = cursor.getString(4);
-        String _teachersNo = cursor.getString(5);
-        String _avgStdPerClass = cursor.getString(6);
-        String _facility = cursor.getString(7);
-
-        CommonModel _commonModel = getCommonModelFromId(_commonId);
-
-        return new EduNewModel(_eduId, _commonModel, _eduType, _shift, _studentNo, _teachersNo, _avgStdPerClass, _facility);
-
+        return new EduNewModel(_commonModel, _edu_type, _shift, _student_no, _teachers_no, _avg_std_per_class, _facility);
     }
 
-    public EduNewModel cursorToModel(Cursor cursor, CommonModel _commonModel) {
+    public ArrayList <EduNewModel> getByAreaCategory(String ward, String area, int category){
+        return super.getByAreaCategory(ward, area, category, TABLE_NAME);
+    }
 
-        int _eduId = cursor.getInt(0);
-        String _eduType = cursor.getString(2);
-        String _shift = cursor.getString(3);
-        String _studentNo = cursor.getString(4);
-        String _teachersNo = cursor.getString(5);
-        String _avgStdPerClass = cursor.getString(6);
-        String _facility = cursor.getString(7);
-
-        return new EduNewModel(_eduId, _commonModel, _eduType, _shift, _studentNo, _teachersNo, _avgStdPerClass, _facility);
-
+    public void delete(int id){
+        super.delete(id, TABLE_NAME);
     }
 
     public void dropTable() {
         super.dropTable(TABLE_NAME);
     }
+
 
 }
