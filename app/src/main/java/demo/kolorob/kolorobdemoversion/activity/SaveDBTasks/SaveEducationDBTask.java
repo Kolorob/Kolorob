93c7e6d6f4d1368c65e6d46c8f314b68b57f6b2d
shamimabrishti <shamima2yasmin@gmail.com>
2017-10-24 08:53:43 +0600

    overridden background process for Education and Health

--- app/src/main/java/demo/kolorob/kolorobdemoversion/activity/SaveDBTasks/SaveEducationDBTask.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/activity/SaveDBTasks/SaveEducationDBTask.java
@@ -3,23 +3,77 @@ package demo.kolorob.kolorobdemoversion.activity.SaveDBTasks;
 import android.content.Context;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import demo.kolorob.kolorobdemoversion.database.Education.EduNewDBTableMain;
+import demo.kolorob.kolorobdemoversion.database.Education.EduNewDBTableSchool;
+import demo.kolorob.kolorobdemoversion.database.Education.EduNewDBTableTraining;
+import demo.kolorob.kolorobdemoversion.database.Education.EducationResultDetailsTable;
 import demo.kolorob.kolorobdemoversion.model.EduNewDB.EduNewModel;
-import demo.kolorob.kolorobdemoversion.utils.AppConstants;
+import demo.kolorob.kolorobdemoversion.model.EduNewDB.EduNewSchoolModel;
+import demo.kolorob.kolorobdemoversion.model.EduNewDB.EduTrainingModel;
+import demo.kolorob.kolorobdemoversion.model.EduNewDB.EducationResultItemNew;
 
 /**
  * Created by shamima.yasmin on 10/17/2017.
  */
 
 
+public class SaveEducationDBTask extends GenericSaveDBTask<JSONArray, Integer, Long, EduNewDBTableMain, EduNewModel> {
 
-public class SaveEducationDBTask extends GenericSaveDBTask <JSONArray, Integer, Long, EduNewDBTableMain, EduNewModel> {
     public SaveEducationDBTask(Context ctx) {
         super(ctx);
     }
 
-    protected Long doInBackground(JSONArray... jsonObjects) {
-        return super.doInBackground(new EduNewDBTableMain(context), new EduNewModel(), AppConstants.EDUCATION, jsonObjects);
+
+    @Override
+    public Long doInBackground(JSONArray... jsonArrays) {
+
+        JSONArray data = jsonArrays[0];
+
+        for (int i = 0; i < data.length(); i++) {
+
+            try {
+                if (!data.isNull(i)) {
+                    JSONObject jsonObject = data.getJSONObject(i);
+
+                    EduNewModel edu = new EduNewModel().parse(jsonObject);
+                    new EduNewDBTableMain(context).insertItem(edu);
+
+                    if (jsonObject.has("training_details")) {
+
+                        JSONArray trainings = jsonObject.getJSONArray("training_details");
+
+                        for (int j = 0; j < trainings.length(); j++) {
+                            JSONObject training = trainings.getJSONObject(j);
+                            new EduNewDBTableTraining(context).insertItem(new EduTrainingModel().parse(training, edu.getId()));
+                        }
+                    }
+                    if (jsonObject.has("result_details")) {
+
+                        JSONArray results = jsonObject.getJSONArray("result_details");
+
+                        for (int j = 0; j < results.length(); j++) {
+                            JSONObject result = results.getJSONObject(j);
+                            new EducationResultDetailsTable(context).insertItem(new EducationResultItemNew().parse(result, edu.getId()));
+                        }
+                    }
+                    if (jsonObject.has("education_school")) {
+                        JSONObject school = jsonObject.getJSONObject("education_school");
+                        new EduNewDBTableSchool(context).insertItem(new EduNewSchoolModel().parse(school, edu.getId()));
+                    }
+
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return new Long(-1);
+            }
+        }
+
+
+        return new Long(0);
+
     }
 }
--- app/src/main/java/demo/kolorob/kolorobdemoversion/activity/SaveDBTasks/SaveHealthDBTask.java
+++ app/src/main/java/demo/kolorob/kolorobdemoversion/activity/SaveDBTasks/SaveHealthDBTask.java
@@ -1,32 +1,60 @@
 package demo.kolorob.kolorobdemoversion.activity.SaveDBTasks;
 
 import android.content.Context;
-import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import demo.kolorob.kolorobdemoversion.activity.DataLoadingActivity;
 import demo.kolorob.kolorobdemoversion.database.Health.HealthNewDBTableHospital;
 import demo.kolorob.kolorobdemoversion.database.Health.HealthNewDBTableMain;
 import demo.kolorob.kolorobdemoversion.database.Health.HealthNewDBTablePharma;
 import demo.kolorob.kolorobdemoversion.model.Health.HealthNewDBModelHospital;
 import demo.kolorob.kolorobdemoversion.model.Health.HealthNewDBModelMain;
 import demo.kolorob.kolorobdemoversion.model.Health.HealthNewDBModelPharmacy;
-import demo.kolorob.kolorobdemoversion.utils.AppConstants;
 
 /**
  * Created by shamima.yasmin on 10/17/2017.
  */
 
-public class SaveHealthDBTask extends GenericSaveDBTask <JSONArray, Integer, Long, HealthNewDBTableMain, HealthNewDBModelMain> {
+public class SaveHealthDBTask extends GenericSaveDBTask<JSONArray, Integer, Long, HealthNewDBTableMain, HealthNewDBModelMain> {
     public SaveHealthDBTask(Context ctx) {
         super(ctx);
     }
 
 
+    @Override
     protected Long doInBackground(JSONArray... jsonArrays) {
-        return super.doInBackground(new HealthNewDBTableMain(context), new HealthNewDBModelMain(), AppConstants.HEALTH, jsonArrays);
+
+        JSONArray data = jsonArrays[0];
+
+        for (int i = 0; i < data.length(); i++) {
+
+            try {
+                if (!data.isNull(i)) {
+                    JSONObject jsonObject = data.getJSONObject(i);
+
+                    HealthNewDBModelMain health = new HealthNewDBModelMain().parse(jsonObject);
+                    new HealthNewDBTableMain(context).insertItem(health);
+
+                    if (jsonObject.has("health_pharmacy")) {
+                        JSONObject pharmacy = jsonObject.getJSONObject("health_pharmacy");
+                        new HealthNewDBTablePharma(context).insertItem(new HealthNewDBModelPharmacy().parse(pharmacy, health.getId()));
+                    }
+                    if (jsonObject.has("health_hospital")) {
+                        JSONObject hospital = jsonObject.getJSONObject("health_hospital");
+                        new HealthNewDBTableHospital(context).insertItem(new HealthNewDBModelHospital().parse(hospital, health.getId()));
+                    }
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return new Long(-1);
+            }
+        }
+
+
+        return new Long(0);
     }
+
 }
